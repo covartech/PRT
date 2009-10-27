2212159e75a845ab84af7cd7348330663151947b
pete <pete@0c11b213-6b92-4ae5-b10c-e53e59125080>
2009-10-27 18:29:06 +0000

    plotNativeMary works now.  We do need to make these Classifiers into objects next.
    git-svn-id: http://svn.newfolderconsulting.com/prt@46 0c11b213-6b92-4ae5-b10c-e53e59125080

--- util/prtPlotNativeMary.m
+++ util/prtPlotNativeMary.m
@@ -9,12 +9,6 @@ if nDims > 3
     error('PrtClassifier.PrtDataSet.nFeatures (%d) must be less than or equal to 3',PrtClassifier.PrtDataSet.nFeatures);
 end
 
-% % Now we remove the PreProcess field so when we run the grid we don't
-% % pre process it.
-% if isfield(PrtClassifier.PrtOptions,'PreProcess')
-%     PrtClassifier.PrtOptions = rmfield(PrtClassifier.PrtOptions,'PreProcess');
-% end
-
 % Make the Meshgrid
 [linGrid,gridSize,xx,yy,zz] = prtPlotUtilGenerateGrid(PrtClassifier,PrtDataSet);
 
@@ -25,7 +19,7 @@ if isfield(PrtClassifier.PrtOptions,'PreProcess')
 end
 
 Results = prtRun(PrtClassifier,linGrid);
-data = Results.y;
+data = Results.getObservations;
 
 PlotOptions = PrtClassifier.PrtOptions.PlotOptions;
 if isfield(PlotOptions,'mappingFunction') && ~isempty(PlotOptions.mappingFunction)
@@ -33,7 +27,6 @@ if isfield(PlotOptions,'mappingFunction') && ~isempty(PlotOptions.mappingFunctio
 end
 
 % So now we got this huge linear grid of data values.
-
 [M,N] = getSubplotDimensions(size(data,2));
 
 classColors = feval(PlotOptions.colorsFunction,size(data,2));
@@ -60,7 +53,7 @@ for i = 1:size(data,2);
         case 2
             imageHandle(i) = imagesc(xx(1,:),yy(:,1),ind2rgb(gray2ind(mat2gray(cDS,[min(data(:)) max(data(:))]),256),linspaceColormap([1 1 1], classColors(i,:)*0.8,256)));
         case 3
-            imageHandle(i) = feval(PlotOptions.displayFunction{NDIM},xx,yy,zz,cDS,max(xx(:)),max(yy(:)),[min(zz(:)),mean(zz(:))]);
+            imageHandle{i} = feval(PlotOptions.displayFunction{nDims},xx,yy,zz,cDS,max(xx(:)),max(yy(:)),[min(zz(:)),mean(zz(:))]);
             view(3)
     end
     axis tight;
