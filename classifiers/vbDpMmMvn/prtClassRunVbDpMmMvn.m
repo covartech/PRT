f15cdfdc7a7bd0b84e246b9473efddb18dfa1c15
kenny <kenny@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-02-11 21:36:38 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@79 0c11b213-6b92-4ae5-b10c-e53e59125080

---
+++ classifiers/vbDpMmMvn/prtClassRunVbDpMmMvn.m
@@ -0,0 +1,17 @@
+function [ClassifierResults,Etc] = prtClassRunVbDpMmMvn(C,DS)
+
+
+%% Pre Proc
+PreProcDS = prtRun(C.PreProc,DS);
+%%
+DecStat = zeros(DS.nObservations,length(C.GmmQs));
+
+for iY = 1:length(C.GmmQs)
+    [logLikes, phiMat] = ezDpMmPriorAlphaToLogLikes(PreProcDS.getObservations(),C.GmmQs{iY});
+    maxLogLikes = max(phiMat,[],2);
+    
+    DecStat(:,iY) = maxLogLikes;
+end
+
+ClassifierResults = prtDataSet(DecStat);
+Etc = [];
