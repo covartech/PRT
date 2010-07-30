2f1cdb79426dc10d4989029b30f0e064aad72f8a
pete <pete@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-07-30 02:51:41 +0000

    added scratch directory; this has some wonky new stuff in it.  some of it might fo in the prt, other stuff will not
    git-svn-id: http://svn.newfolderconsulting.com/prt@163 0c11b213-6b92-4ae5-b10c-e53e59125080

---
+++ scratch/prtFeatureExtractor.m
@@ -0,0 +1,3 @@
+classdef prtFeatureExtractor < prtAction
+    
+end
---
+++ scratch/prtFeatureExtractorTfcmGpr.m
@@ -0,0 +1,56 @@
+classdef prtFeatureExtractorTfcmGpr < prtFeatureExtractor
+    
+    
+    properties (SetAccess = private)
+
+        name = 'Texture feature coding method';
+    	nameAbbreviation = 'TFCM';
+    	isSupervised = false;
+    
+        threshold = 1;
+        distance = [1 1];
+    end
+    
+    methods (Access = protected)
+        
+        function obj = trainAction(obj,varargin)
+            %do nothing
+        end
+        function prtDataSetOut = runAction(obj,prtDataSet)
+            %[Features,y,ASLinked] = nfAraProcessAlarmSet(obj,AS,OS,GprPreProcessOptions)
+            
+            for iAlarm = 1:prtDataSet.nObservations
+                
+                data = prtDataSet.getObservations(iAlarm);
+                Alarm = prtDataSet.getAlarms(iAlarm);
+                while (size(data,3) < 61)
+                    data = cat(3,data,data(:,:,end));
+                end
+                disp('not pre-processing');
+                %dataPreProc = GprPreProcessOptions.preProcessingFunction(data);
+                theImage = squeeze(data(:,Alarm.Info.crossTrack,:));
+                
+                textureFeats(iAlarm,:) = obj.extractTfcm(theImage);
+                
+                disp(iAlarm);
+            end
+            prtDataSetOut = prtDataSetClass(textureFeats,prtDataSet.getTargets);
+        end
+
+        function [textureFeats,textureImage] = extractTfcm(obj,theImage)
+            %[textureFeats,textureImage] = extractTfcm(obj)
+            
+            theImage = medfilt2(theImage);
+            [textureFeats,textureImage] = image2textureFeatures(theImage,obj.threshold,obj.distance);
+        end
+        
+        %         function [textureFeats,textureImage] = plot(obj)
+        %             %[textureFeats,textureImage] = plot(obj)
+        %             [textureFeats,textureImage] = extractTfcm(obj);
+        %
+        %             subplot(2,1,1); imagesc(obj.theImage);
+        %             subplot(2,1,2); imagesc(textureImage);
+        %         end
+    end
+end
+        
