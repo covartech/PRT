5529273e30b6515a4dcd5d3050430cbc5ab9033d
sam <sam@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-08-04 02:03:56 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@176 0c11b213-6b92-4ae5-b10c-e53e59125080

---
+++ test/prtTestClassMAP.m
@@ -0,0 +1,71 @@
+function result = prtTestClassMAP
+result = true;
+
+% BASELINE generation, uncomment to run to generate new baseline
+% Run numIter times to get idea distribution of percentage
+% Pick off the lowest % correct and use that as baseline
+% numIter = 1000;
+% percentCorr = zeros(1,numIter);
+% for i = 1:numIter
+%     TestDataSet = prtDataUniModal;
+%     TrainingDataSet = prtDataUniModal;
+%
+%     classifier = prtClassMAP;
+%     classifier = classifier.train(TrainingDataSet);
+%     classified = run(classifier, TestDataSet);
+%     classes  = classified.data > .5;
+%     percentCorr(i) = prtScorePercentCorrect(classes,TestDataSet.getTargets);
+% end
+% min(percentCorr)
+
+
+%% Classification correctness test.
+baselinePercentCorr = .9475;
+
+TestDataSet = prtDataUniModal;
+TrainingDataSet = prtDataUniModal;
+
+classifier = prtClassMAP;
+classifier = classifier.train(TrainingDataSet);
+classified = run(classifier, TestDataSet);
+%classes  = classified.data > .5;
+classes  = classified.getX > .5;
+
+percentCorr = prtScorePercentCorrect(classes,TestDataSet.getTargets);
+
+result = result & (percentCorr > baselinePercentCorr);
+
+%% Error checks
+
+error = true;  % We will want all these things to error
+
+classifier = prtClassMAP;
+
+try
+    classifier.rvs = 1;
+    error = false;  % Set it to false if the preceding operation succeeded
+catch
+    % do nothing
+    % We can potentially catch and check the error string here
+    % For now, just be happy it is erroring out.
+end
+
+
+%% Object construction
+% We want these to be non-errors
+noerror = true;
+
+try
+    classifier = prtClassMap('rvs', prtRvMvn);
+catch
+    noerror = false;
+end
+
+try 
+    classifier = prtClassMap;
+    classifier.rvs = prtRvMvn;
+catch
+    noerror = false;
+end
+%% 
+result = result & error & noerror;
