d40eedf1de3544aeaff5c151f387468f5375fc73
sam <sam@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-08-13 02:10:52 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@200 0c11b213-6b92-4ae5-b10c-e53e59125080

--- test/prtTestFeatSelExhaustive.m
+++ test/prtTestFeatSelExhaustive.m
@@ -4,12 +4,65 @@ function result = prtTestFeatSelExhaustive
 result = true;
 %%
 
-DS = prtDataUnimodal;
-DS = DS.setObservations([DS.getObservations,DS.getObservations + randn(size(DS.getObservations))*10]);
 
-opt = prtFeatSelOptExhaustive;
-opt.nFeatures = 4;
 
-Algo = prtGenerate(DS,opt);
+%%
+% This should not error out.
+try
+dataSet = prtDataCircles;
+featSel = prtFeatSelExhaustive; 
+featSel.nFeatures = 1;
+featSel = featSel.train(dataSet);
+outDataSet = featSel.run(dataSet);
+catch
+    disp('Error #1, basic feature selection fail')
+    result = false;
+end
 
-R = prtRun(Algo,DS);
+if outDataSet.nFeatures ~=1
+    disp('Error #2, wrong # of features')
+    result = false;
+end
+
+if featSel.isTrained ~= 1
+    disp('Error #2a, should be trained')
+    result = false;
+end
+
+
+% Check changing the Eval metric
+dataSet = prtDataCircles;
+featSel = prtFeatSelExhaustive; 
+featSel.nFeatures = 1;
+try
+featSel.EvaluationMetric = @(DS)prtScorePdAtPf(DS, prtClassMAP, .9);
+featSel = featSel.train(dataSet);
+outDataSet = featSel.run(dataSet);
+catch
+    disp('Error #3, change eval metric')
+    result = false;
+end
+
+% check param/value constructor
+try
+    featSel = prtFeatSelExhaustive('nFeatures',2, 'EvaluationMetric', @(DS)prtScorePdAtPf(DS, prtClassMAP, .9));
+catch
+    disp('Error #4, param/val constructor fail')
+    result = false;
+end
+%% Stuff that should error
+error = true;
+% This errors out messily if you don't train first, should error out clean.
+dataSet = prtDataSpiral3;
+featSel = prtFeatSelExhaustive; 
+featSel.nFeatures = 1;
+
+try
+    R = featSel.run(dataSet);
+    disp('Error# x , run before train')
+    error = false;
+end
+
+result = result & error;
+
+
