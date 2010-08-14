7a99b4dff045b8a6ab80bcac42a43084852e9a29
sam <sam@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-08-14 01:42:28 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@205 0c11b213-6b92-4ae5-b10c-e53e59125080

---
+++ test/prtTestPreProcZmuv.m
@@ -0,0 +1,21 @@
+function result = prtTestPreProcZmuv
+result = true;
+try
+    dataSet = prtDataProstate;       % Load a data set.
+    zmuv = prtPreProcZmuv;           %  Create a zero-mean unit variance
+    %  object
+    zmuv = zmuv.train(dataSet);      % Compute the mean and variance
+    dataSetNew = zmuv.run(dataSet);  % Normalize the data
+catch
+    disp('basic zmuv failure')
+    result = false;
+end
+if  abs(mean(dataSetNew.getObservations())) > 1e-13
+    result = false;
+    disp('zmuv mean not zero')
+end
+
+if  abs(1-var(dataSetNew.getObservations())) > 1e-13      %Check the variance
+    result = false;
+    disp('zmuv variance not 1');
+end
