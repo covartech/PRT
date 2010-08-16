cbce05e0da3ac7f32be1bf9ead642eca3e3bb96b
sam <sam@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-08-16 21:09:16 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@208 0c11b213-6b92-4ae5-b10c-e53e59125080

---
+++ test/prtTestFeatSelStatic.m
@@ -0,0 +1,57 @@
+function result = prtTestFeatSelStatic
+
+%%
+result = true;
+%%
+
+
+
+%%
+% Check basic operation
+try
+dataSet = prtDataCircles;
+featSel = prtFeatSelStatic; 
+featSel.selectedFeatures = 1;
+featSel = featSel.train(dataSet);
+outDataSet = featSel.run(dataSet);
+catch
+    disp('Error #1, basic feature selection fail')
+    result = false;
+end
+
+if outDataSet.nFeatures ~=1
+    disp('Error #2, wrong # of features')
+    result = false;
+end
+
+if featSel.isTrained ~= 1
+    disp('Error #2a, should be trained')
+    result = false;
+end
+
+if ~isequal(outDataSet.getX, dataSet.getFeatures(1));
+    disp('Error # 3, wrong feature selected')
+end
+
+try
+    featSel = prtFeatSelExhaustive('selectedFeatures',2);
+catch
+    disp('Error #4, param/val constructor fail')
+    result = false;
+end
+%% Stuff that should error
+error = true;
+% This errors out messily if you don't train first, should error out clean.
+dataSet = prtDataSpiral3;
+featSel = prtFeatSelStatic; 
+
+
+try
+    R = featSel.run(dataSet);
+    disp('Error# 5 , run without setting features')
+    error = false;
+end
+
+result = result & error;
+
+
