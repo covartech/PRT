48c8242207c52b4666e61bab6349afdad09c75e0
sam <sam@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-08-17 03:38:52 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@220 0c11b213-6b92-4ae5-b10c-e53e59125080

--- distance/prtDistanceCityBlock.m
+++ distance/prtDistanceCityBlock.m
@@ -1,31 +1,25 @@
-function D = prtDistanceCityBlock(varargin)
-% CITYBLOCK      Calculate the distance from all of the points in P1 to all
-%   of the points in P2 usuing the cityblock distance measure. The
-%   cityblock distance is the sum of the absolute distances between each of
-%   the dimensions of P1 and P2.
+function D = prtDistanceCityBlock(p1,p2)
+% prtDistanceCityBlock   City block distance
+% 
+%    DIST = prtDistanceCityBlock(P1,P2) Calculate the distance from all of
+%    the points in P1 to all of the points in P2 usuing the cityblock
+%    distance measure. The cityblock distance is the sum of the absolute
+%    distances between each of the dimensions of P1 and P2.
+% 
+%   P1 is a  NxM matrix of locations. N is the number of points and M is
+%   the dimensionality. P2 is a DxM matrix of locations. D is the number of
+%   points and M is the dimensionality. The output DIST is a NxD matrix of
+%   distances.
 %
-% Syntax: D = cityblock(varargin)
 %
-% Inputs: 
-%   varargin{1} - double Mat - NxM matrix of locations. N is the number of 
-%       points and M is the dimensionality.
-%   varargin{2} - double Mat - DxM matrix of locations. D is the number of 
-%       points and M is the dimensionality.
-%
-% Outputs
-%   D - NxD matrix of distances.
-%
-% Example:
-%   X = [0 0; 1 1];
-%   Y = [1 0; 2 2; 3 3;];
-%   D = dprtDistanceCityBlock(X,Y)
+%    Example:
+%      X = [0 0; 1 1];
+%      Y = [1 0; 2 2; 3 3;];
+%      D = prtDistanceCityBlock(X,Y)
 %   
-% Other m-files required: none
-% Subfunctions: none
-% MAT-files required: none
-%
-% See also: distance.m chebychev.m euclidean.m lnorm.m mahalanobis.m
-%   squaredist.m
+% See also: prtDistance, prtDistanceMahalanobis, prtDistanceLNorm.
+% prtDistanceEuclidean, prtDistanceSquare, prtDistanceChebychev
+
 
 % Author: Kenneth D. Morton Jr.
 % Duke University, Department of Electrical and Computer Engineering
@@ -33,12 +27,12 @@ function D = prtDistanceCityBlock(varargin)
 % Created: 17-December-2005
 % Last revision: 5-January-2006
 
-%D = prtDistanceCustom(varargin{1},varargin{2},@(x1,x2)sum(abs(x1-x2)));
+D = prtDistanceCustom(p1,p2,@(x1,x2)sum(abs(x1-x2)));
 
-nDims = size(varargin{1},2);
+nDims = size(p1,2);
 
 for iDim = 1:nDims
-    cD = prtDistanceLNorm(varargin{1}(:,iDim),varargin{2}(:,iDim),1);
+    cD = prtDistanceLNorm(p1(:,iDim),p2(:,iDim),1);
     if iDim == 1
         D = cD;
     else
