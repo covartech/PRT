b9e2d495dea7878dde73945aaaab60a411e1acc5
pete <pete@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-08-17 15:58:03 +0000

    added prtDataPca and prtDataFeatSel
    git-svn-id: http://svn.newfolderconsulting.com/prt@231 0c11b213-6b92-4ae5-b10c-e53e59125080

--- dataset/prtDataSetClass.m
+++ dataset/prtDataSetClass.m
@@ -360,7 +360,7 @@ classdef prtDataSetClass  < prtDataSetStandard
         function varargout = plot(obj, featureIndices)
             
             if ~obj.isLabeled
-                obj = obj.setTargets(0);
+                obj = obj.setTargets(zeros(obj.nObservations,1));
                 obj = obj.setClassNames({'Unlabeled'});
             end
             
