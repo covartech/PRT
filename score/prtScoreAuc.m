465bdb1bc097a204a4beaf76d66a190c2f2ed4c3
sam <sam@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-08-18 04:02:15 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@238 0c11b213-6b92-4ae5-b10c-e53e59125080

--- score/prtScoreAuc.m
+++ score/prtScoreAuc.m
@@ -1,5 +1,16 @@
 function auc = prtScoreAuc(DS,PrtClassObj)
-%auc = prtScoreAuc(DS,PrtClassOpt)
-
+%prtScoreAuc   Score by the area under curve method
+%
+%   AUC = prtScoreAuc(DS,PRTCLASSOBJ) returns the area under the receiver
+%   operating curve. DS must be a labeled, binary prtDataSetStandard
+%   object. PRTCLASSOBJ must be a prtClass object.
+%
+%   Example:
+%   dataSet = prtDataSpiral;
+%   classifier = prtClassDlrt;
+%   prtScoreAuc(dataSet,classifier)
+%
+%   See Also: prtScoreAucKfolds, prtScorePdAtPf, prtScorePdAtPfKfolds,
+%   prtScorePfAtPd, prtScorePfAtPdKfolds, prtScoreRoc
 Results = run(PrtClassObj.train(DS),DS);
 [~,~,auc] = prtScoreRoc(Results.getObservations,DS.getTargets);
--- score/prtScoreAucKfolds.m
+++ score/prtScoreAucKfolds.m
@@ -1,5 +1,19 @@
 function auc = prtScoreAucKfolds(DS,PrtClass,nFolds)
-%auc = prtScoreAucKfolds(DS,PrtClassOpt,nFolds)
+%prtScoreAucKfolds   Score by the area under the receiver operating curve
+%method with K-fold cross-validation.
+%
+%   AUC = prtScoreAucKfolds(DS,PRTCLASSOBJ,NFOLDS) returns the area under
+%   the receiver operating curve. DS must be a labeled, binary
+%   prtDataSetStandard object. PRTCLASSOBJ must be a prtClass object.
+%   NFOLDS is the number of folds in the K-fold cross-validation.
+%
+%   Example:
+%   dataSet = prtDataSpiral;
+%   classifier = prtClassDlrt;
+%   prtScoreAucKfolds(dataSet,classifier,10)
+%
+%   See Also: prtScoreAuc, prtScorePdAtPf, prtScorePdAtPfKfolds,
+%   prtScorePfAtPd, prtScorePfAtPdKfolds, prtScoreRoc
 
 Results = kfolds(PrtClass,DS,nFolds);
 [~,~,auc] = prtScoreRoc(Results.getObservations,DS.getTargets);
