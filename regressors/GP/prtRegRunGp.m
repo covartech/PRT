0e343ade88fed2b521e459fe57b305ec80f312be
pete <pete@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-08-21 21:56:15 +0000

    removed old GP, LSLR, and RVM regression algos
    git-svn-id: http://svn.newfolderconsulting.com/prt@268 0c11b213-6b92-4ae5-b10c-e53e59125080

--- regressors/GP/prtRegRunGp.m
+++
@@ -1,9 +0,0 @@
-function ClassifierResults = prtRegRunGp(Gp,DS)
-
-k = feval(Gp.covarianceFunction, Gp.PrtDataSet.getObservations(), DS.getObservations());
-
-ClassifierResults = prtDataSet(k'*Gp.weights);
-
-c = diag(feval(Gp.covarianceFunction, DS.getObservations(), DS.getObservations())) + Gp.noiseVariance;
-
-ClassifierResults.UserData.variances = c - prtUtilCalcDiagXcInvXT(k', Gp.CN);
