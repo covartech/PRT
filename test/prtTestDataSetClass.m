40f68391080359f9ab5ba2c0e03bb243706610f1
sam <sam@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-08-25 19:55:05 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@284 0c11b213-6b92-4ae5-b10c-e53e59125080

---
+++ test/prtTestDataSetClass.m
@@ -0,0 +1,119 @@
+function result = prtTestDataSetClass
+result = true;
+
+% Test instantiation 
+try
+    dataSet = prtDataSetClass;
+    dataSet = dataSet.setObservationsAndTargets([ 1 2; 3 4], [0 1]');
+catch
+    disp('Instantiation of prtDataSetClass fail')
+    result = false;
+end
+
+% Check the results of above
+if(dataSet.nClasses ~= 2)
+    disp('prtDataSet nClasses fail')
+    result = false;
+end
+
+if(dataSet.isZeroOne == 1)
+    disp('isZeroOne fail')
+    result = false;
+end
+
+if(dataSet.nFeatures ~=2 || dataSet.nTargetDimensions ~=1 || dataSet.isLabeled ~=1)
+    disp('prtDataSet setup fail')
+    result = false;
+end
+    
+% Check functions
+if ~isequal(dataSet.getObservationsByClass(0),  [1 2]);
+    disp('Get obs by class fail')
+    result = false;
+end
+
+
+if ~isequal(dataSet.getObservationsByClassInd(2,2),  4);
+    disp('Get obs by class and ind fail')
+    result = false;
+end
+
+if ~isequal(dataSet.getTargetsAsBinaryMatrix, eye(2))
+    disp('getTargets Binary Matrix fail')
+    result = false;
+end
+
+% make sure the explorer, plots, etc open without error
+try
+    dataSet.explore;
+    close
+catch
+    disp('dataSetClass explore fail')
+    close;
+    result = false;
+end
+
+try
+    dataSet.plot;
+    close
+catch
+    disp('dataSetClass plot fail')
+    close;
+    result = false;
+end
+
+try
+    dataSet.plotbw;
+    close
+catch
+    disp('dataSetClass plotbw fail')
+    close;
+    result = false;
+end
+
+try
+    dataSet.plotAsTimeSeries;
+    close
+catch
+    disp('dataSetClass plot as time series fail')
+    close;
+    result = false;
+end
+
+try
+    dataSet.starPlot
+    close
+catch
+    disp('dataSetClass star plot as time series fail')
+    close;
+    result = false;
+end
+
+
+% Test bootstrap
+dataSet = prtDataMary;
+out = dataSet.bootstrap(2);
+if out.nObservations ~=2
+    disp('dataSet bootstrap fail')
+    result = false;
+end
+
+dataSet = dataSet.setObservationsAndTargets([ 1 2; 3 4], [0 1]');
+% out = dataSet.bootstrapByClass(ones(dataSet.nObservations));
+% if out.nObservations ~=$ 
+%     disp('dataSet bootstrap fail')
+%     result = false;
+% end
+
+if ~isequal(dataSet.nObservationsByClass, [1 1]')
+    disp('dataSetClass n Obs by class fail')
+    result = false;
+end
+
+
+out = dataSet.getTargetsClassInd(1);
+if out ~=1
+    disp('dataSetClass getTargetsClassInd fail')
+    result = false;
+end
+
