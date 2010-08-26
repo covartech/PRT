2892466ef61b61d013bcc39396caeab0ca664baa
sam <sam@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-08-26 21:04:12 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@315 0c11b213-6b92-4ae5-b10c-e53e59125080

--- test/prtTestClassGlrt.m
+++ test/prtTestClassGlrt.m
@@ -1,4 +1,4 @@
-function result = prtTestClassMAP
+function result = prtTestClassGlrt
 result = true;
 
 % BASELINE generation, uncomment to run to generate new baseline
@@ -9,23 +9,23 @@ result = true;
 % for i = 1:numIter
 %     TestDataSet = prtDataGenUnimodal;
 %     TrainingDataSet = prtDataGenUnimodal;
-%
-%     classifier = prtClassMAP;
+% 
+%     classifier = prtClassGlrt;
 %     classifier = classifier.train(TrainingDataSet);
 %     classified = run(classifier, TestDataSet);
-%    classes  = classified.getX > .5;
+%     classes  = classified.getX > .5;
 %     percentCorr(i) = prtScorePercentCorrect(classes,TestDataSet.getTargets);
 % end
 % min(percentCorr)
 
 
 %% Classification correctness test.
-baselinePercentCorr = .9475;
+baselinePercentCorr = .9400;
 
 TestDataSet = prtDataGenUnimodal;
 TrainingDataSet = prtDataGenUnimodal;
 
-classifier = prtClassMAP;
+classifier = prtClassGlrt;
 classifier = classifier.train(TrainingDataSet);
 classified = run(classifier, TestDataSet);
 
@@ -33,12 +33,16 @@ classes  = classified.getX > .5;
 
 percentCorr = prtScorePercentCorrect(classes,TestDataSet.getTargets);
 
-result = result & (percentCorr > baselinePercentCorr);
+if (percentCorr < baselinePercentCorr)
+    disp('prtClassGlrt below baseline')
+    result = false;
+end
+
 
 %% Check that cross-val and k-folds work
 
 TestDataSet = prtDataGenUnimodal;
-classifier = prtClassMAP;
+classifier = prtClassGlrt;
 
 % cross-val
 keys = mod(1:400,2);
@@ -46,26 +50,31 @@ crossVal = classifier.crossValidate(TestDataSet,keys);
 classes  = crossVal.getX > .5;
 percentCorr = prtScorePercentCorrect(classes,TestDataSet.getTargets);
 
-result = result & (percentCorr > baselinePercentCorr);
+if (percentCorr < baselinePercentCorr)
+    disp('prtClassGlrt cross-val below baseline')
+    result = false;
+end
 
 % k-folds
 
 crossVal = classifier.kfolds(TestDataSet,10);
 classes  = crossVal.getX > .5;
 percentCorr = prtScorePercentCorrect(classes,TestDataSet.getTargets);
-
-result = result & (percentCorr > baselinePercentCorr);
-
+if (percentCorr < baselinePercentCorr)
+    disp('prtClassGlrt kfolds below baseline')
+    result = false;
+end
 
 %% Error checks
 
 error = true;  % We will want all these things to error
 
-classifier = prtClassMAP;
+classifier = prtClassGlrt;
 
 try
-    classifier.rvs = 1;
+    classifier.rvH0 = 1;
     error = false;  % Set it to false if the preceding operation succeeded
+    disp('Set rvH0 to non prt Rv')
 catch
     % do nothing
     % We can potentially catch and check the error string here
@@ -78,16 +87,18 @@ end
 noerror = true;
 
 try
-    classifier = prtClassMap('rvs', prtRvMvn);
+    classifier = prtClassGlrt('rvH1', prtRvMvn);
 catch
     noerror = false;
+    disp('GLRT param/val constructor fail');
 end
 
 try 
-    classifier = prtClassMap;
-    classifier.rvs = prtRvMvn;
+    classifier = prtClassGlrt;
+    classifier.rvH0 = prtRvMvn;
 catch
     noerror = false;
+    disp('GLRT set rvH0 fail')
 end
 %% 
 result = result & error & noerror;
