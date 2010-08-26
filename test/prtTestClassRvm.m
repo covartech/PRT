54a0c6f821d81ae9626457f2f1c14bc58300d733
sam <sam@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-08-26 02:03:03 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@295 0c11b213-6b92-4ae5-b10c-e53e59125080

---
+++ test/prtTestClassRvm.m
@@ -0,0 +1,110 @@
+function result = prtTestClassRvm
+result = true;
+
+% BASELINE generation, uncomment to run to generate new baseline
+% Run numIter times to get idea distribution of percentage
+% Pick off the lowest % correct and use that as baseline
+% numIter = 100;
+% percentCorr = zeros(1,numIter);
+% for i = 1:numIter
+%     TestDataSet = prtDataUniModal;
+%     TrainingDataSet = prtDataUniModal;
+%
+%     classifier = prtClassRvm;
+%     classifier = classifier.train(TrainingDataSet);
+%     classified = run(classifier, TestDataSet);
+%     classes  = classified.getX > .5;
+%     percentCorr(i) = prtScorePercentCorrect(classes,TestDataSet.getTargets);
+% end
+% min(percentCorr)
+
+
+%% Classification correctness test.
+baselinePercentCorr =  0.9375;
+
+TestDataSet = prtDataUniModal;
+TrainingDataSet = prtDataUniModal;
+
+classifier = prtClassRvm;
+%classifier.verboseStorage = false;
+classifier = classifier.train(TrainingDataSet);
+classified = run(classifier, TestDataSet);
+
+classes  = classified.getX > .5;
+
+percentCorr = prtScorePercentCorrect(classes,TestDataSet.getTargets);
+
+result = result & (percentCorr > baselinePercentCorr);
+
+% make sure plotting succeeds
+try
+    classifier.plot();
+    close;
+catch
+    result = false;
+    disp('Rvm class plot fail')
+    close
+end
+
+
+%% Check that cross-val and k-folds work
+
+TestDataSet = prtDataUniModal;
+classifier = prtClassRvm;
+
+% cross-val
+keys = mod(1:400,2);
+crossVal = classifier.crossValidate(TestDataSet,keys);
+classes  = crossVal.getX > .5;
+percentCorr = prtScorePercentCorrect(classes,TestDataSet.getTargets);
+
+result = result & (percentCorr > baselinePercentCorr);
+
+% k-folds
+
+crossVal = classifier.kfolds(TestDataSet,10);
+classes  = crossVal.getX > .5;
+percentCorr = prtScorePercentCorrect(classes,TestDataSet.getTargets);
+
+result = result & (percentCorr > baselinePercentCorr);
+
+% check that i can change the algorithm
+try
+    classifier.algorithm = 'Sequential';
+    classifier = classifier.train(TrainingDataSet);
+    classified = run(classifier, TestDataSet);
+    
+    classifier.algorithm = 'SequentialInMemory';
+    classifier = classifier.train(TrainingDataSet);
+    classified = run(classifier, TestDataSet);
+catch
+    result = false;
+    disp('error changing algoritm Rvm classifier')
+end
+
+
+
+%check learning plot and learning text
+try
+    classifier.LearningPlot = true;
+    classifier.LearningText = true;
+    classifier.train(TestDataSet);
+    classified = run(classifier, TestDataSet);
+    close all;
+catch
+    disp('Rvm learning plot/text fail')
+    result = false
+end
+
+
+%% Object construction
+% We want these to be non-errors
+noerror = true;
+
+try
+    classifier = prtClassRvm('LearningMaxIterations', 40);
+catch
+    noerror = false;
+end
+% %%
+result = result & noerror;
