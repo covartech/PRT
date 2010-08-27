f9b07f656227147706db38b32dec28a43e4b0d99
sam <sam@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-08-27 01:24:14 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@324 0c11b213-6b92-4ae5-b10c-e53e59125080

---
+++ test/prtTestClassCap.m
@@ -0,0 +1,108 @@
+function result = prtTestClassCap
+result = true;
+
+% BASELINE generation, uncomment to run to generate new baseline
+% Run numIter times to get idea distribution of percentage
+% Pick off the lowest % correct and use that as baseline
+% numIter = 1000;
+% percentCorr = zeros(1,numIter);
+% for i = 1:numIter
+%     TestDataSet = prtDataGenUnimodal;
+%     TrainingDataSet = prtDataGenUnimodal;
+% 
+%     classifier = prtClassCap;
+%     classifier = classifier.train(TrainingDataSet);
+%     classified = run(classifier, TestDataSet);
+%     classes  = classified.getX > .5;
+%     percentCorr(i) = prtScorePercentCorrect(classes,TestDataSet.getTargets);
+% end
+% min(percentCorr)
+
+
+%% Classification correctness test.
+baselinePercentCorr = .9425;
+
+TestDataSet = prtDataGenUnimodal;
+TrainingDataSet = prtDataGenUnimodal;
+
+classifier = prtClassCap;
+classifier = classifier.train(TrainingDataSet);
+classified = run(classifier, TestDataSet);
+
+classes  = classified.getX > .5;
+
+percentCorr = prtScorePercentCorrect(classes,TestDataSet.getTargets);
+
+if (percentCorr < baselinePercentCorr)
+    disp('prtClassCap below baseline')
+    result = false;
+end
+
+
+%% Check that cross-val and k-folds work
+
+TestDataSet = prtDataGenUnimodal;
+classifier = prtClassCap;
+
+% cross-val
+keys = mod(1:400,2);
+crossVal = classifier.crossValidate(TestDataSet,keys);
+classes  = crossVal.getX > .5;
+percentCorr = prtScorePercentCorrect(classes,TestDataSet.getTargets);
+
+if (percentCorr < baselinePercentCorr)
+    disp('prtClassCap cross-val below baseline')
+    result = false;
+end
+
+% k-folds
+
+crossVal = classifier.kfolds(TestDataSet,10);
+classes  = crossVal.getX > .5;
+percentCorr = prtScorePercentCorrect(classes,TestDataSet.getTargets);
+if (percentCorr < baselinePercentCorr)
+    disp('prtClassCap kfolds below baseline')
+    result = false;
+end
+
+%% Error checks
+
+error = true;  % We will want all these things to error
+
+classifier = prtClassCap;
+
+try
+    classifier.w = 1;
+    error = false;  % Set it to false if the preceding operation succeeded
+    disp('prtClassCap Set w sucessful ')
+catch
+    % do nothing
+    % We can potentially catch and check the error string here
+    % For now, just be happy it is erroring out.
+end
+
+
+try
+    classifier.threshold = 1;
+    error = false;  % Set it to false if the preceding operation succeeded
+    disp('prtClassCap Set w sucessful ')
+catch
+    % do nothing
+    % We can potentially catch and check the error string here
+    % For now, just be happy it is erroring out.
+end
+
+%% Object construction
+% We want these to be non-errors
+noerror = true;
+
+try
+    classifier = prtClassCap('thresholdSampling', 50);
+catch
+    noerror = false;
+    disp('Cap param/val constructor fail');
+end
+
+
+%% 
+result = result & error & noerror;
