24b60db1142182212ea20f492b55c857ca15529c
pete <pete@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-08-28 16:13:48 +0000

    added prtClassMatlabTreeBagger; this is an interface to matlab's treebagging class in the stats toolbox
    git-svn-id: http://svn.newfolderconsulting.com/prt@353 0c11b213-6b92-4ae5-b10c-e53e59125080

---
+++ classifiers/prtClassMatlabTreeBagger.m
@@ -0,0 +1,51 @@
+classdef prtClassMatlabTreeBagger < prtClass
+    %DS = prtDataGenBimodal;
+    %Nn = prtClassMatlabTreeBagger; 
+    % %Or:
+    %Nn = prtClassMatlabTreeBagger('treeBaggerParamValuePairs',{'nVarToSample','all'});
+    % %or
+    %Nn = prtClassMatlabTreeBagger('treeBaggerParamValuePairs',{'nVarToSample',2});
+    %Nn = Nn.train(DS);
+    %Nn.plot; 
+    %yOut = Nn.run(prtDataGenBimodal);
+    
+    properties (SetAccess=private)
+        
+        name = 'MATLAB Tree Bagger' % Fisher Linear Discriminant
+        nameAbbreviation = 'MLTB'            % FLD
+        isSupervised = true;   % True
+        isNativeMary = true;  % False
+    end
+    
+    properties 
+        % forest
+        nTrees = 50;
+        forest
+        treeBaggerParamValuePairs
+    end
+
+    methods 
+               % Allow for string, value pairs
+        function Obj = prtClassMatlabTreeBagger(varargin)
+            Obj = prtUtilAssignStringValuePairs(Obj,varargin{:});
+        end
+        
+        function Obj = set.nTrees(Obj,val)
+            assert(isscalar(val) && isnumeric(val) && all(val == round(val)) && all(val > 0),'prt:prtClassMatlabTreeBagger:nTrees','nTrees must be a numeric scalar int-valued double greater than 0, but value provided was %s',mat2str(val));
+            Obj.nTrees = val;
+        end
+    end
+    
+    methods (Access=protected, Hidden = true)
+        
+        function Obj = trainAction(Obj,DataSet)
+            Obj.forest = treeBagger(Obj.nTrees,DataSet.getObservations,DataSet.getTargets,Obj.treeBaggerParamValuePairs{:});
+        end
+        
+        function DataSet = runAction(Obj,DataSet)
+            [~,scores] = predict(Obj.forest,DataSet.getObservations);
+            DataSet = DataSet.setObservations(scores);
+        end
+        
+    end
+end
