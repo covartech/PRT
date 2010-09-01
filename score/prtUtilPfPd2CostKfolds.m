df8ad6dc99e16512ad925fa1ae93f69c12b27b8c
pete <pete@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-09-01 01:10:30 +0000

    added prtUtilPfPd2Cost
    git-svn-id: http://svn.newfolderconsulting.com/prt@373 0c11b213-6b92-4ae5-b10c-e53e59125080

--- score/prtUtilPfPd2CostKfolds.m
+++
@@ -1,7 +0,0 @@
-function cost = prtUtilPfPd2CostKfolds(DS,PrtClass,nFolds,costMatrix)
-%cost = prtUtilPfPd2CostKfolds(DS,PrtClassOpt,nFolds)
-
-Results = kfolds(PrtClass,DS,nFolds);
-[pf,pd] = prtScoreRoc(Results.getObservations,DS.getTargets);
-cost = prtUtilPfPd2Cost(pf,pd,costMatrix);
-cost = min(cost);
