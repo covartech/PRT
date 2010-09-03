d4c4f6a68f83e98f81c0bb7c82dbb4926037d99a
sam <sam@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-09-03 02:56:20 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@382 0c11b213-6b92-4ae5-b10c-e53e59125080

---
+++ test/prtTestScore.m
@@ -0,0 +1,47 @@
+function result = prtTestScore
+% This function tests a few of the prtScore functions such as:
+% prtScorePercentCorrect
+
+result = true;
+
+%% Positive checks, these should work
+
+% try binary labels
+guess = [0 1 1 0]';
+truth = [1 0 1 0]';
+
+if(prtScorePercentCorrect(guess, truth) ~= .5)
+    result = false;
+    disp('prtScorePercent binary correct wrong answer')
+end
+
+% Try m-ary
+guess = [0 1 2 3 ]';
+truth = [0 1 4 3]';
+if(prtScorePercentCorrect(guess, truth) ~= .5)
+    result = false;
+    disp('prtScorePercentM-ary correct wrong answer')
+end
+
+%%  These things should error 
+error = true;
+
+guess = [0 1 1 0];
+truth = [1 0 1 0];
+try 
+    prtScorePercentCorrect(guess,truth);
+    error = false;
+    disp('prtScorePercentCorrect wrong orientation')
+catch
+   % no-op
+end
+
+guess = [0 1 1 0];
+truth = [1 0 1 1 0];
+try 
+    prtScorePercentCorrect(guess,truth);
+    error = false;
+    disp('prtScorePercentCorrect wrong orientation')
+catch
+   % no-op
+end
