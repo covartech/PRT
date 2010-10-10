489aff34008a4d0c4bac173cda124c775861d3d6
sam <sam@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-10-10 00:19:06 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@476 0c11b213-6b92-4ae5-b10c-e53e59125080

--- test/prtTestScore.m
+++ test/prtTestScore.m
@@ -29,43 +29,43 @@ if(prtScorePercentCorrect(guess, truth) ~= .75)
     disp('prtScorePercentM-ary correct wrong answer')
 end
 
-%%  These things should error 
+%%  These things should error
 error = true;
 
 guess = [0 1 1 0];
 truth = [1 0 1 0];
-try 
+try
     prtScorePercentCorrect(guess,truth);
     error = false;
     disp('prtScorePercentCorrect wrong orientation')
 catch
-   % no-op
+    % no-op
 end
 
-try 
+try
     prtScoreRmse(guess,truth);
     error = false;
     disp('prtScoreRmse wrong orientation')
 catch
-   % no-op
+    % no-op
 end
 
 
 guess = [0 1 1 0]';
 truth = [1 0 1 1 0]';
-try 
+try
     prtScorePercentCorrect(guess,truth);
     error = false;
     disp('prtScorePercentCorrect wrong size')
 catch
-   % no-op
+    % no-op
 end
-try 
+try
     prtScoreRmse(guess,truth);
     error = false;
     disp('prtScoreRmse wrong size')
 catch
-   % no-op
+    % no-op
 end
 %% test prtScoreCost
 
@@ -79,7 +79,86 @@ C = prtScoreCost(classified.getX, TestDataSet.getY, [1 .1; .1 1]);
 
 if (C ~= .55 )
     disp('prtScoreCost wrong value')
-    result = false; 
+    result = false;
 end
+
+
+%% test prtScoreRoc etc...
+TestDataSet = prtDataGenSpiral;       % Create some test and
+TrainingDataSet = prtDataGenSpiral;   % training data
+classifier = prtClassGlrt;             % Create a classifier
+classifier = classifier.train(TrainingDataSet);    % Train
+classified = run(classifier, TestDataSet);
+
+% Ensure basic operation doesn't fail
+try
+    prtScoreRoc(classified.getX(), TrainingDataSet.getY())
+    close;
+catch
+    result = false;
+    disp('prtScoreRoc basic fail')
+end
+
+% Ensure basic operation doesn't fail
+try
+    [nfa] = prtScoreRocNfa(classified.getX(), TrainingDataSet.getY());
+    
+catch
+    result = false;
+    disp('prtScoreRocNfa basic fail')
+end
+
+if(min(nfa) ~= 0) || (max(nfa) ~= 200)
+    result = false;
+    disp('prtScoreNfa wrong ans')
+end
+
+
+try
+    prtScoreRocBayesianBootstrap(classified.getX(), TrainingDataSet.getY(), 100,100,5);
+    
+    close;
+    disp('prtScoreRocBayesianBootstrap check alpha fail')
+    result = false;
+catch
+    
+    close
+end
+
+
+try
+    prtScoreRocBayesianBootstrapNfa(classified.getX(), TrainingDataSet.getY(), 100,100,.1);
+    close;
+catch
+    result = false;
+    disp('prtScoreRocBayesianBootstrapNfa basic fail')
+end
+
+
+% Ensure basic operation doesn't fail
+try
+    [nfa] = prtScoreRocBayesianBootstrapNfa(classified.getX(), TrainingDataSet.getY(), 100,100,.1);
+    
+catch
+    result = false;
+    disp('prtScoreRocBayesianBoostrapNfa basic fail')
+end
+
+if(min(nfa) ~= 0) || (max(nfa) ~= 200)
+    result = false;
+    disp('prtScoreBayesianBootstapNfa wrong ans')
+end
+
+try
+    prtScoreRocBayesianBootstrapNfa(classified.getX(), TrainingDataSet.getY(), 100,100,5);
+    close;
+    result = false;
+    disp('prtScoreRocBayesianBootstrap check alpha fail')
+catch
+    close
+    
+end
+
+
 
 
