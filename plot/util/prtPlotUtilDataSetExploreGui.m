ad1b48d1b356bf75946b4213cf5528e6ee0c1c58
pete <pete@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-10-14 02:09:50 +0000

    in explorer, left clicking in the axes now tells you the index of the point closest to your last click
    git-svn-id: http://svn.newfolderconsulting.com/prt@482 0c11b213-6b92-4ae5-b10c-e53e59125080

--- plot/util/prtPlotUtilDataSetExploreGui.m
+++ plot/util/prtPlotUtilDataSetExploreGui.m
@@ -75,11 +75,69 @@ updatePlot;
     function updatePlot
         actualPlotDims = plotDims(plotDims>=1);
         axes(axisH); %#ok
-        plot(theObject,actualPlotDims)
+        h = plot(theObject,actualPlotDims);
+        set(h,'HitTest','off');
+        set(axisH,'ButtonDownFcn',@axisOnClick);
     end
     function explorerPopOut(myHandle,eventData) %#ok
         figure;
         actualPlotDims = plotDims(plotDims>=1);
         plot(theObject,actualPlotDims);
+    end
+    function axisOnClick(myHandle,eventData)
+        actualPlotDims = plotDims(plotDims>=1);
+        data = theObject.getFeatures(actualPlotDims);
+        
+        [rP,rD] = rotateDataAndClick(data);
+        
+        dist = prtDistanceEuclidean(rP,rD);
+        [~,i] = min(dist);
+        title(sprintf('Observation Closest To Last Click: %d',i));
+        
+        debug = false;
+        if debug
+            hold on;
+            d = theObject.getObservations(i);
+            switch length(actualPlotDims)
+                case 2
+                    plot(d(1),d(2),'kx');
+                case 3
+                    plot3(d(1),d(2),d(3),'kx');
+            end
+            hold off;
+        end
+    end
+    
+    function [rotatedData,rotatedClick] = rotateDataAndClick(data)
+        %[rotatedData,rotatedClick] = rotateDataAndClick(data)
+        % Used internally; from Click3dPoint from matlab central; need
+        % copyright here.
+        
+        point = get(gca, 'CurrentPoint'); % mouse click position
+        camPos = get(gca, 'CameraPosition'); % camera position
+        camTgt = get(gca, 'CameraTarget'); % where the camera is pointing to
+        
+        camDir = camPos - camTgt; % camera direction
+        camUpVect = get(gca, 'CameraUpVector'); % camera 'up' vector
+        
+        % build an orthonormal frame based on the viewing direction and the
+        % up vector (the "view frame")
+        zAxis = camDir/norm(camDir);
+        upAxis = camUpVect/norm(camUpVect);
+        xAxis = cross(upAxis, zAxis);
+        yAxis = cross(zAxis, xAxis);
+        
+        rot = [xAxis; yAxis; zAxis]; % view rotation
+        
+        if size(data,2) < 3
+            data = cat(2,data,zeros(size(data,1),1));
+        end
+        
+        % the point cloud represented in the view frame
+        rotatedData = (rot * data')';
+        rotatedData = rotatedData(:,1:2);
+        % the clicked point represented in the view frame
+        rotatedClick = rot * point' ;
+        rotatedClick = rotatedClick(1:2,1)';
     end
 end
