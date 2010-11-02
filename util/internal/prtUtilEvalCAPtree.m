181ed792adcea123d3830a96ceca42cfe8a1ca5b
kenny <kenny@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-11-02 01:32:05 +0000

    synced prtUtilEvalCAPtree
    git-svn-id: http://svn.newfolderconsulting.com/prt@536 0c11b213-6b92-4ae5-b10c-e53e59125080

--- util/internal/prtUtilEvalCAPtree.m
+++ util/internal/prtUtilEvalCAPtree.m
@@ -3,23 +3,6 @@ function Yout = prtUtilEvalCAPtree(tree,X,nClasses)
 %   Evaluate a CAP tree on a 1xN data point X
 % Internal 
 % xxx Need Help xxx
-% 
-% index = 1;
-% voted = 0;
-% while ~voted
-%     if any(isfinite(tree.W(:,index)))
-%         %disp(((tree.W(:,index)'*X(:,tree.featureIndices(:,index))')') - tree.threshold(:,index))
-%         Yout = double(((tree.W(:,index)'*X(:,tree.featureIndices(:,index))')' - tree.threshold(:,index)) >= 0);
-%         if Yout == 0
-%             index = find(tree.treeIndices(:) == index,1,'first');
-%         elseif Yout > 0
-%             index = find(tree.treeIndices(:) == index,1,'last');
-%         end
-%     else
-%         Yout = tree.terminalVote(index);
-%         voted = 1;
-%     end
-% end
 
 if nargin < 3 || isempty(nClasses)
     nClasses = 2;
--- util/prtInternal/prtUtilEvalCAPtree.m
+++ util/prtInternal/prtUtilEvalCAPtree.m
@@ -1,11 +1,15 @@
-function Yout = prtUtilEvalCAPtree(tree,X)
+function Yout = prtUtilEvalCAPtree(tree,X,nClasses)
 %Yout = evalCAPtree(tree,X)
 %   Evaluate a CAP tree on a 1xN data point X
 % Internal 
 % xxx Need Help xxx
 
+if nargin < 3 || isempty(nClasses)
+    nClasses = 2;
+end
+
 index = 1;
-voted = 0;
+voted = false;
 while ~voted
     if any(isfinite(tree.W(:,index)))
         %disp(((tree.W(:,index)'*X(:,tree.featureIndices(:,index))')') - tree.threshold(:,index))
@@ -16,8 +20,8 @@ while ~voted
             index = find(tree.treeIndices(:) == index,1,'last');
         end
     else
-        Yout = tree.terminalVote(index);
-        voted = 1;
+        Yout = zeros(1,nClasses);
+        Yout(tree.terminalVote(index)) = 1;
+        voted = true;
     end
 end
-
