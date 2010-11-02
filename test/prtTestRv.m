16a0445e5b8b677cee3422bf0697cce1da7929f9
kenny <kenny@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-11-02 19:40:41 +0000

    prtTestRv now works.
    git-svn-id: http://svn.newfolderconsulting.com/prt@549 0c11b213-6b92-4ae5-b10c-e53e59125080

--- test/prtTestRv.m
+++ test/prtTestRv.m
@@ -1,136 +1,132 @@
-function prtTestRv
-%% MVN
-R = prtRvMvn;
-R = R.mle(randn(10,2));
-R = prtRvMvn('covarianceStructure','diagonal');
-R = prtRvMvn('covarianceStructure','spherical');
-R = R.mle(bsxfun(@plus,mvnrnd([0 1],[1 0.2; 0.2 2],100),[10 5]));
+function result = prtTestRv
 
-plotPdf(R)
-%% Multinomial (Discrete)
+result = true;
 
-R = prtRvMultinomial;
-R = R.mle([100 90 10]');
-R = prtRvMultinomial(R.draw(100));
-
-plotCdf(R)
-
-%% Mixture with MVN 2D
-
-N(1) = prtRvMvn('Mean',1*[-1 -1],'Covariance',[2 -0.9; -0.9 1]);
-N(2) = prtRvMvn('Mean',1*[1 1],'Covariance',[1 0.2; 0.2 2]);
-
-GMM = prtRvMixture('components',N,'mixingProportions',prtRvMultinomial('probabilities',[0.7 0.3]));
-
-plotPdf(GMM)
-title('Truth')
-
-[X,Y] = GMM.draw(1000);
-LGMM = prtRvMixture(X,'components',repmat(prtRvMvn,2,1));
-
-figure
-plotPdf(LGMM);
-title('Learned');
-hold on;
-plot(prtDataSetClass(X,Y));
-hold off;
-
-%% Mixture with MVN 1D
-
-N(1) = prtRvMvn('Mean',-19,'Covariance',2);
-N(2) = prtRvMvn('Mean',1,'Covariance',1);
-
-GMM = prtRvMixture('components',N,'mixingProportions',prtRvMultinomial('probabilities',[0.7 0.3]));
-
-plotCdf(GMM)
-title('Truth')
-
-[X,Y] = GMM.draw(1000);
-LGMM = prtRvMixture(X,'components',repmat(prtRvMvn,2,1));
-
-figure
-plotCdf(LGMM);
-title('Learned')
-
-%% GMM
-
-LGMM2 = prtRvGmm(X,'nComponents',2);
-
-plotPdf(LGMM2)
-title('GMM')
-%%
-
-R = prtRvUniform('lowerBounds',[0 0],'upperBounds',[1 1]);
-
-plotCdf(R,[-2 4 0 5])
-
-%%
-
-R = prtRvUniform(draw(prtRvMvn('Mean',[1 2],'Covariance',2*eye(2)),100));
-
-plotPdf(R)
-%%
-
-R = prtRvUniformImproper(draw(prtRvMvn('Mean',[1 2],'Covariance',2*eye(2)),100));
-plotPdf(R)
-
-%% 
-
-R = prtRvDiscrete('symbols',(10:12)','probabilities',[0.3 0.3 0.4]);
-
-R2 = R.mle(R.draw(1000));
-plotCdf(R2);
-
-%%
-
-R = prtRvDiscrete('symbols',(10:12)','probabilities',[0.3 0.3 0.4]);
-
-R2 = R.mle(R.draw(1000));
-plotCdf(R2);
-
-R.pdf(R.draw(1000))
-%%
-
-R = prtRvDiscrete('symbols',randn(3,10),'probabilities',[0.3 0.3 0.4]);
-
-R2 = R.mle(R.draw(1000));
-plotCdf(R2);
-
-R.pdf(R.draw(1000))
-%%
-
-N(1) = prtRvMvn('Mean',1*[-1 -1],'Covariance',[2 -0.9; -0.9 1]);
-N(2) = prtRvMvn('Mean',1*[1 1],'Covariance',[1 0.2; 0.2 2]);
-
-GMM = prtRvMixture('components',N,'mixingProportions',prtRvMultinomial('probabilities',[0.4 0.6]));
-
-plotPdf(GMM)
-title('Truth')
-
-[X,Y] = GMM.draw(1000);
-
-
-LGMM2 = prtRvGmm(X,'nComponents',2,'covariancePool',true,'covarianceStructure','diag');
-
-plotPdf(LGMM2)
-title('GMM')
-hold on;
-plot(prtDataSetClass(X,Y));
-hold off;
-%%
-
-
-N(1) = prtRvMvn('Mean',1*[-1 -1],'Covariance',[2 -0.9; -0.9 1]);
-N(2) = prtRvMvn('Mean',1*[1 1],'Covariance',[1 0.2; 0.2 2]);
-
-GMM = prtRvMixture('components',N,'mixingProportions',prtRvMultinomial('probabilities',[0.4 0.6]));
-
-plotPdf(GMM)
-title('Truth')
-
-[X,Y] = GMM.draw(1000);
-%%
-
-R = prtRvVq(X,'nCategories',100);
-
-R.pdf(R.draw(10))
+try
+    figure
+    
+    %% MVN
+    R = prtRvMvn;
+    R = mle(prtRvMvn,randn(10,2));
+    R.covarianceStructure = 'diagonal';
+    
+    plotPdf(R)
+    %% Multinomial (Discrete)
+    
+    R = prtRvMultinomial;
+    R = mle(prtRvMultinomial,[100 90 10]);
+    R = mle(prtRvMultinomial,R.draw(100));
+    
+    plotPdf(R)
+    
+    %% Mixture with MVN 2D
+    
+    N(1) = prtRvMvn('Mean',1*[-1 -1],'Covariance',[2 -0.9; -0.9 1]);
+    N(2) = prtRvMvn('Mean',1*[1 1],'Covariance',[1 0.2; 0.2 2]);
+    
+    GMM = prtRvMixture('components',N,'mixingProportions',prtRvMultinomial('probabilities',[0.7 0.3]));
+    
+    plotPdf(GMM)
+    title('Truth')
+    
+    [X,Y] = GMM.draw(1000);
+    LGMM = mle(prtRvMixture('components',repmat(prtRvMvn,2,1)),X);
+    
+    plotPdf(LGMM);
+    title('Learned');
+    hold on;
+    plot(prtDataSetClass(X,Y));
+    hold off;
+    
+    %% Mixture with MVN 1D
+    
+    N(1) = prtRvMvn('Mean',-19,'Covariance',2);
+    N(2) = prtRvMvn('Mean',1,'Covariance',1);
+    
+    GMM = prtRvMixture('components',N,'mixingProportions',prtRvMultinomial('probabilities',[0.7 0.3]));
+    
+    plotCdf(GMM)
+    title('Truth')
+    
+    [X,Y] = GMM.draw(1000);
+    LGMM = mle(prtRvMixture('components',repmat(prtRvMvn,2,1)),X);
+    
+    plotCdf(LGMM);
+    title('Learned')
+    
+    %% GMM
+    
+    LGMM2 = mle(prtRvGmm('nComponents',2),X);
+    
+    plotPdf(LGMM2)
+    title('GMM')
+    %%
+    
+    R = prtRvUniform('lowerBounds',[0 0],'upperBounds',[1 1]);
+    
+    plotCdf(R,[-2 4 0 5])
+    
+    %%
+    
+    R = mle(prtRvUniform,draw(prtRvMvn('Mean',[1 2],'Covariance',2*eye(2)),100));
+    
+    plotPdf(R)
+    %%
+    
+    R = mle(prtRvUniformImproper,draw(prtRvMvn('Mean',[1 2],'Covariance',2*eye(2)),100));
+    plotPdf(R)
+    
+    %%
+    
+    R = prtRvDiscrete('symbols',(10:12)','probabilities',[0.3 0.3 0.4]);
+    
+    R2 = R.mle(R.draw(1000));
+    plotPdf(R2);
+    
+    %%
+    
+    R = mle(prtRvDiscrete,draw(prtRvMvn('Mean',[1 2],'Covariance',2*eye(2)),100));
+    
+    plotPdf(R);
+    
+    %%
+    
+    N(1) = prtRvMvn('Mean',1*[-1 -1],'Covariance',[2 -0.9; -0.9 1]);
+    N(2) = prtRvMvn('Mean',1*[1 1],'Covariance',[1 0.2; 0.2 2]);
+    
+    GMM = prtRvMixture('components',N,'mixingProportions',prtRvMultinomial('probabilities',[0.4 0.6]));
+    
+    plotPdf(GMM)
+    title('Truth')
+    
+    [X,Y] = GMM.draw(1000);
+    
+    
+    LGMM2 = mle(prtRvGmm('nComponents',2,'covariancePool',true,'covarianceStructure','diag'),X);
+    
+    plotPdf(LGMM2)
+    title('GMM')
+    hold on;
+    plot(prtDataSetClass(X,Y));
+    hold off;
+    %%
+    
+    
+    N(1) = prtRvMvn('Mean',1*[-1 -1],'Covariance',[2 -0.9; -0.9 1]);
+    N(2) = prtRvMvn('Mean',1*[1 1],'Covariance',[1 0.2; 0.2 2]);
+    
+    GMM = prtRvMixture('components',N,'mixingProportions',prtRvMultinomial('probabilities',[0.4 0.6]));
+    
+    plotPdf(GMM)
+    title('Truth')
+    
+    [X,Y] = GMM.draw(1000);
+    %%
+    
+    R = mle(prtRvVq('nCategories',100),X);
+    
+    R.pdf(R.draw(10));
+    close;
+catch
+    result = false;
+end
