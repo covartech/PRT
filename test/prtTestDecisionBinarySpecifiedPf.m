fb39d65c16c3becd9c82d4295ebd36fe94ae6902
sam <sam@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-12-09 02:38:52 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@630 0c11b213-6b92-4ae5-b10c-e53e59125080

---
+++ test/prtTestDecisionBinarySpecifiedPf.m
@@ -0,0 +1,73 @@
+function prtTestDecisionBinarySpecifiedPf
+result = true;
+
+dsTrain = prtDataGenUnimodal;
+dsTest = prtDataGenUnimodal;
+
+% Try the algorithm technique
+try
+    algo = prtClassKnn;
+    dec  =  prtDecisionBinarySpecifiedPf;
+    dec.pf =.5;
+    algo = algo + dec;
+    algo = algo.train(dsTrain);
+    outAlgo = algo.run(dsTest);
+catch
+    result = false;
+    disp('prtDecisionBinarySpecifiedPf algo fail')
+end
+
+try
+    myKnn = prtClassKnn;
+    myKnn.internalDecider = prtDecisionBinarySpecifiedPf;
+    myKnn.internalDecider.pf = .5;
+    myKnn = myKnn.train(dsTrain);
+    outIntDec = myKnn.run(dsTest);
+catch
+    result = false;
+    disp('prtDecisionBinarySpecifiedPf internal dec fail')
+    result = false;
+end
+
+% check that the results are the same
+% Which are the class labels????
+if ~isequal(outAlgo.getX, outIntDec.getX)
+    result = false;
+    disp('prtDecisionBinarySpecifiedPf algo/int not equal')
+end
+
+% check that plot works in both cases
+try
+    algo.plot;
+    close all;
+catch
+    disp('prtDecisionBinarySpecPf algo plot fail');
+    close all
+    result = false;
+end
+
+
+% check that plot works in both cases
+try
+    myKnn.plot;
+    close all;
+catch
+    disp('prtDecisionBinarySpecPf internal decider plot fail');
+    close all
+    result = false;
+end
+
+% check that errors when pf not set
+try
+    
+    dsTrain = prtDataGenUnimodal;
+    dsTest = prtDataGenUnimodal;
+    myKnn = prtClassKnn;
+    myKnn.internalDecider = prtDecisionBinarySpecifiedPf;
+    myKnn = myKnn.train(dsTrain);
+    outIntDec = myKnn.run(dsTest);
+    disp('prtDecisionBinarySpecPf runs with unset pf')
+    result = false;
+catch
+    %noop
+end
