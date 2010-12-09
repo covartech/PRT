ae9789619080efcf3eab707969b1de2ce2cd34d4
pete <pete@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-12-09 15:41:26 +0000

    fixed prtHistEq
    git-svn-id: http://svn.newfolderconsulting.com/prt@631 0c11b213-6b92-4ae5-b10c-e53e59125080

--- preProcessing/prtPreProcHistEq.m
+++ preProcessing/prtPreProcHistEq.m
@@ -48,7 +48,10 @@ classdef prtPreProcHistEq < prtPreProc
     end
     
     methods
-        
+        function obj = set.nSamples(obj,var)
+            assert(prtUtilIsPositiveInteger(var),'Error, nSamples must be a positive integer');
+            obj.nSamples = var;
+        end
         function Obj = prtPreProcHistEq(varargin)
             % Allow for string, value pairs
             % There are no user settable options though.
@@ -60,16 +63,14 @@ classdef prtPreProcHistEq < prtPreProc
         
         function Obj = trainAction(Obj,DataSet)
             
-            if Obj.nSamples == inf;
+            if Obj.nSamples == inf
                 Obj.nSamples = DataSet.nObservations;
                 for dim = 1:DataSet.nFeatures
-                    %Obj.binEdges{dim} = sort(DataSet.getX);
-                    [~,Obj.binEdges] =sort(DataSet.getX);
+                    Obj.binEdges = sort(DataSet.getX);
                 end
             else
                 for dim = 1:DataSet.nFeatures
-                    %[~,Obj.binEdges{dim}] = hist(DataSet.getFeatures(dim),Obj.nSamples);
-                    [~,Obj.binEdges(:,dim)] = hist(DataSet.getObservations(:,dim),Obj.nSamples);
+                    [~,Obj.binEdges(:,dim)] = hist(DataSet.getFeatures(dim),Obj.nSamples);
                 end
             end
             
@@ -81,15 +82,31 @@ classdef prtPreProcHistEq < prtPreProc
             
             X = zeros(DataSet.nObservations,DataSet.nFeatures);
             for index = 1:DataSet.nObservations
-                [ii,jj] = find(bsxfun(@gt,DataSet.getObservations(index,:),Obj.binEdges));
-                [~,i] = unique(jj);
-                ii = ii(i);
-                X(index,:) = ii';
+                
+                %Find everywhere where any column is greater than binEdges
+                %(centers)?
+                [ii,jj] = find(bsxfun(@le,DataSet.getObservations(index,:),Obj.binEdges));
+                %keyboard
+                %The indices of gthe unique jj's are the first time the
+                %gt test passed; these are the places we care about
+                [uniqueJJ,firstInd] = unique(jj,'first');
+                
+                if length(uniqueJJ) < DataSet.nFeatures
+                    nanInd = setdiff(1:DataSet.nFeatures,uniqueJJ);
+                    X(index,nanInd) = nan;
+                end
+                
+                if ~isempty(firstInd)
+                    iiSelected = ii(firstInd);
+                    X(index,uniqueJJ) = iiSelected';
+                end
             end
+            
+            %Right now we have the histogram index, normalize this to be
+            %between 0 and 1
             X = X./(size(Obj.binEdges,1)-2);  %-2, one for first, and one for last bin
             DataSet = DataSet.setObservations(X);
         end
         
     end
-    
 end
