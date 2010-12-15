84352e33c90d696fb2469d14823244cf328b09c5
sam <sam@0c11b213-6b92-4ae5-b10c-e53e59125080>
2010-12-15 20:32:43 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@635 0c11b213-6b92-4ae5-b10c-e53e59125080

---
+++ test/prtTestDecisionMap.m
@@ -0,0 +1,54 @@
+function prtTestDecisionMap
+result = true;
+
+dsTrain = prtDataGenMary;
+dsTest = prtDataGenMary;
+
+% Try the algorithm technique
+try
+    algo = prtClassKnn + prtDecisionMap;
+    algo = algo.train(dsTrain);
+    outAlgo = algo.run(dsTest);
+catch
+    result = false;
+    disp('prtTestDecisionMap algo fail')
+end
+
+try
+    myKnn = prtClassKnn;
+    myKnn.internalDecider = prtDecisionMap;
+    myKnn = myKnn.train(dsTrain);
+    outIntDec = myKnn.run(dsTest);
+catch
+    result = false;
+    disp('prtTestDecisionMap internal dec fail')
+    result = false;
+end
+
+% check that the results are the same
+% Which are the class labels????
+if ~isequal(outAlgo.getX, outIntDec.getX)
+    result = false;
+    disp('prtTestDecisionMap algo/int not equal')
+end
+
+% check that plot works in both cases
+try
+    algo.plot;
+    close all;
+catch
+    disp('prtDecisionMap algo plot fail');
+    close all
+    result = false;
+end
+
+
+% check that plot works in both cases
+try
+    myKnn.plot;
+    close all;
+catch
+    disp('prtDecisionMap internal decider plot fail');
+    close all
+    result = false;
+end
