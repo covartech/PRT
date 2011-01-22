a319c4d8bfa4619e1dd060a81abde323196651e8
sam <sam@0c11b213-6b92-4ae5-b10c-e53e59125080>
2011-01-22 01:42:09 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@660 0c11b213-6b92-4ae5-b10c-e53e59125080

--- test/prtTestOutlierRemoveMissingData.m
+++ test/prtTestOutlierRemoveMissingData.m
@@ -1,40 +1,32 @@
-function result = prtTestPreProcNstdOutlierRemove
+function result = prtTestOutlierRemoveMissingData
 result = true;
 
 % test basic operation
 try
     dataSet = prtDataGenUnimodal;               % Load a data set
-    outlier = prtDataSetClass([-10 -10],1);     % Insert an outlier
+    outlier = prtDataSetClass([NaN NaN],1);     % Insert an outlier
     dataSet = catObservations(dataSet,outlier); % Concatenate
     
-    nStdRemove = prtOutlierRemovalNStd;  % Create a prtPreProc Object
+    nStdRemove = prtOutlierRemovalMissingData;  % Create a prtPreProc Object
     nStdRemove.runMode = 'removeObservation';
     nStdRemove = nStdRemove.train(dataSet);    % Train
     dataSetNew = nStdRemove.run(dataSet);      % Run
 catch
     result = false;
-    disp('prtPreProcNstdOutlierRemove basic fail')
+    disp('prtdOutlierRemove Missing data basic fail')
 end
 
 % check that the outlier was actually removed
 if dataSetNew.nObservations ~= dataSet.nObservations-1
-    disp('prtPreProcNstdOutlierRemove did not remove outlier')
+    disp('prtdOutlierRemoveMissing data did not remove outlier')
     result = false;
 end
 
-% check the other modes
-nStdRemove.runMode = 'replaceWithNan';
-dataSetNew = nStdRemove.run(dataSet);      % Run
-x = dataSetNew.getX;
-if ~all(isnan(x(end,:)))
-    disp('pre remove outlier nstd did not replace data with nan')
-    result = false;
-end
 
 
 % check remove feature mode, so only make 1 feature an outlier
 dataSet = prtDataGenUnimodal;               % Load a data set
-outlier = prtDataSetClass([-10 1],1);     % Insert an outlier
+outlier = prtDataSetClass([NaN 1],1);     % Insert an outlier
 dataSet = catObservations(dataSet,outlier); % Concatenate
 nStdRemove.runMode = 'removeFeature';
 dataSetNew = nStdRemove.run(dataSet);      % Run
