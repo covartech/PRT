ee9c96974d1df5990f3d4d1a686d925c1283d5c3
kenny <kenny@0c11b213-6b92-4ae5-b10c-e53e59125080>
2011-02-02 16:43:05 +0000

    Massive cleaning of the repo.
    git-svn-id: http://svn.newfolderconsulting.com/prt@733 0c11b213-6b92-4ae5-b10c-e53e59125080

--- beta/rv/prtRvUtilLaplaceRnd.m
+++
@@ -1,5 +0,0 @@
-function X = prtRvUtilLaplaceRnd(mu,theta,N)
-% X = laplacernd(mu,theta,N)
-
-U = rand(N,1)-1/2;
-X = mu - theta .* sign(U) .* log(1-2*abs(U));
--- beta/rv/prtRvUtilStudentTRnd.m
+++
@@ -1,6 +0,0 @@
-function x = prtRvUtilStudentTRnd(mu,Sigma,dof,N)
-% x = studenttrnd(mu,Sigma,dof,N)
-
-warning('This is not really a student T')
-
-x = mvnrnd(mu,Sigma,N);
