14a9ecfbcf969feac44e45fdd4d7e73545905408
kenny <kenny@0c11b213-6b92-4ae5-b10c-e53e59125080>
2011-02-02 21:33:55 +0000

    isSupervised is dead!
    git-svn-id: http://svn.newfolderconsulting.com/prt@746 0c11b213-6b92-4ae5-b10c-e53e59125080

---
+++ preProc/prtPreProcClass.m
@@ -0,0 +1,8 @@
+classdef prtPreProcClass < prtPreProc
+    methods
+        function obj = prtPreProcClass()
+            obj.classInput = 'prtDataSetClass';
+            obj.classOutput = 'prtDataSetClass';
+        end
+    end
+end
