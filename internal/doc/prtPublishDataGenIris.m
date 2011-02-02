b55570b9bf22f1a1709f500bfede66998b7eca26
kenny <kenny@0c11b213-6b92-4ae5-b10c-e53e59125080>
2011-02-02 16:57:13 +0000

    Massive cleaning of the repo.
    git-svn-id: http://svn.newfolderconsulting.com/prt@736 0c11b213-6b92-4ae5-b10c-e53e59125080

---
+++ internal/doc/prtPublishDataGenIris.m
@@ -0,0 +1,34 @@
+%% prtDataGenIris
+% This data generation method can be used to load the classic Iris data
+% set.  The Iris data set consists of measurements of sepal and petal
+% lengths and widths from 3 different types of Iris flowers.  For more
+% information and references, please see:
+% <http://en.wikipedia.org/wiki/Iris_flower_data_set>.
+%
+% The output of this function is a prtDataSetClass.
+%
+% <matlab:doc('prtDataGenIris') M-file documentation>
+%
+% <prtPublishDataGen.html prtDataGen Functions>
+%
+% <prtPublishGettingStarted.html Getting Started> 
+%
+
+ds = prtDataGenIris;
+explore(ds);
+
+%%
+% See also: 
+% <prtPublishDataGenBimodal.html prtDataGenBimodal>
+% <prtPublishDataGenCircles.html prtDataGenCircles>
+% <prtPublishDataGenIris.html prtDataGenIris>
+% <prtPublishDataGenManual.html prtDataGenManual>
+% <prtPublishDataGenMary.html prtDataGenMary>
+% <prtPublishDataGenNoisySinc.html prtDataGenNoisySinc>
+% <prtPublishDataGenOldFaithful.html prtDataGenOldFaithful>
+% <prtPublishDataGenProstate.html prtDataGenProstate>
+% <prtPublishDataGenSpiral.html prtDataGenSpiral>
+% <prtPublishDataGenSpiral3.html prtDataGenSpiral3>
+% <prtPublishDataGenSwissRoll.html prtDataGenSwissRoll>
+% <prtPublishDataGenUnimodal.html prtDataGenUnimodal>
+% <prtPublishDataGenXor.html prtDataGenXor>
---
+++ internal/doc/prtPublishDataGenManual.m
@@ -0,0 +1,32 @@
+%% prtDataGenManual
+%
+% This data generation method can be used to manually define a binary or
+% M-ary classification data set by clicking the mouse in a figure axes.
+% Use the left-mouse button to add a point at the current mouse location in
+% the current hypothesis (default hypothesis is 0), and use the right mouse
+% button to cycle through hypotheses.  
+%
+% The output of this function is a prtDataSetClass.
+%
+% <matlab:doc('prtDataGenManual') M-file documentation>
+%
+% <prtPublishDataGen.html prtDataGen Functions>
+%
+% <prtPublishGettingStarted.html Getting Started> 
+%
+
+%%
+% See also: 
+% <prtPublishDataGenBimodal.html prtDataGenBimodal>
+% <prtPublishDataGenCircles.html prtDataGenCircles>
+% <prtPublishDataGenIris.html prtDataGenIris>
+% <prtPublishDataGenManual.html prtDataGenManual>
+% <prtPublishDataGenMary.html prtDataGenMary>
+% <prtPublishDataGenNoisySinc.html prtDataGenNoisySinc>
+% <prtPublishDataGenOldFaithful.html prtDataGenOldFaithful>
+% <prtPublishDataGenProstate.html prtDataGenProstate>
+% <prtPublishDataGenSpiral.html prtDataGenSpiral>
+% <prtPublishDataGenSpiral3.html prtDataGenSpiral3>
+% <prtPublishDataGenSwissRoll.html prtDataGenSwissRoll>
+% <prtPublishDataGenUnimodal.html prtDataGenUnimodal>
+% <prtPublishDataGenXor.html prtDataGenXor>
