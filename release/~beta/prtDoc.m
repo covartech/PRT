98cee12f0e2c61ba7e92e680f7b8407d17a20761
kenny <kenny@0c11b213-6b92-4ae5-b10c-e53e59125080>
2011-02-08 14:34:07 +0000

    Deleting non-release things.
    git-svn-id: http://svn.newfolderconsulting.com/prt@796 0c11b213-6b92-4ae5-b10c-e53e59125080

--- release/~beta/prtDoc.m
+++
@@ -1,9 +0,0 @@
-function prtDoc(varargin)
-% prtDoc prt Documentation Shortcut
-%
-
-if nargin
-    doc(varargin{:});
-else
-    web(fullfile(prtRoot,'doc','html','prtPublishGettingStarted.html'))
-end
--- release/~scratch/vbDpMmLogDiscBag/prtClassRunVbDpMmLogDiscBag.m
+++
@@ -1,8 +0,0 @@
-function [ClassifierResults,Etc] = prtClassRunVbDpMmLogDiscBag(C,DS)
-%%
-
-[y, yParts] = logDiscBagPredictiveProb(DS.getObservations,C.Q);
-
-ClassifierResults = prtDataSet(y);
-
-Etc.clusterProbs = yParts;
