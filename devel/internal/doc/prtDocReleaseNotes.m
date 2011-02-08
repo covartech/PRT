07306f06e7d28b7e9b65f590a654a5f8190b49da
pete <pete@0c11b213-6b92-4ae5-b10c-e53e59125080>
2011-02-08 23:31:51 +0000

    added some doc
    git-svn-id: http://svn.newfolderconsulting.com/prt@835 0c11b213-6b92-4ae5-b10c-e53e59125080

--- devel/internal/doc/prtDocReleaseNotes.m
+++ devel/internal/doc/prtDocReleaseNotes.m
@@ -1,7 +1,7 @@
 %% Pattern Recognition Toolbox Release Notes
 % 
 %
-%% Version 1.0, 09-Feb-2011
+%% Version 0.0.1(Beta), 09-Feb-2011
 % Initial beta release. For updates visit
 % <http://www.newfolderconsulting.com/prt/>.
 %
