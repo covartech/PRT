0e415fa1c13ccde19ba61bd06fa627634c5fdbcf
kenny <kenny@0c11b213-6b92-4ae5-b10c-e53e59125080>
2011-03-24 13:29:07 +0000

    packaging... Updating Contents.m
    git-svn-id: http://svn.newfolderconsulting.com/prt@917 0c11b213-6b92-4ae5-b10c-e53e59125080

--- devel/Contents.m
+++ devel/Contents.m
@@ -1,2 +1,2 @@
 % Pattern Recognition Toolbox
-% Version 915 24-Mar-2011
+% Version 916 24-Mar-2011
--- devel/internal/prtPackage.m
+++ devel/internal/prtPackage.m
@@ -2,6 +2,7 @@ function prtPackage(prtTarget)
 % prtPackage - Package the release of the PRT into a zip file.
 
 %Write the Contents.m file in the current directory
+disp('Updating contents .m');
 oldDir = pwd;
 cd(prtRoot);
 [v,s] = system('svn info');
@@ -11,6 +12,7 @@ version = s{1}{12};
 writeContents(prtRoot,version);
 
 % Commit this new Contents.m
+disp('Committing to repo...');
 prtVerCtrl('commit','packaging... Updating Contents.m');
 
 % Merge Devel and Release and Create a Release branch marked with the
@@ -21,6 +23,7 @@ develSvnRoot = 'http://svn.newfolderconsulting.com/prt/devel';
 releaseSvnRoot = 'http://svn.newfolderconsulting.com/prt/release';
 
 % Merge
+disp('Merging with release trunk...');
 system(sprintf('svn merge %s %s %s --ignore-ancestry',develSvnRoot,releaseSvnRoot,prtReleaseRoot));
 % Mark conflicts
 system(sprintf('svn resolve -R --accept=working %s',prtReleaseRoot));
@@ -29,11 +32,11 @@ system(sprintf('svn commit %s -m "merging for package"',prtReleaseRoot));
 
 
 % Branch
+disp(cat(2,'Creating release branch ',mat2str(version),'...'));
 newSvnRoot = sprintf('http://svn.newfolderconsulting.com/prt/release%s',version);
 system(sprintf('svn copy %s %s -m "package"',releaseSvnRoot,newSvnRoot));
 
 % Ok now are ready to actually package things up
-
 if nargin == 0
     prtTarget = fullfile(getenv('userprofile'),'desktop','prt');
 end
@@ -44,18 +47,21 @@ if isdir(prtTarget)
 end
    
 % SVN Export
+disp('Exporting with release branch...');
 local = prtReleaseRoot;
 target = prtTarget;
 command = sprintf('svn export "%s" "%s"',local,target);
 system(command);
 
 % P-Code Files
+disp('P-Coding select files...');
 filesToPCode = cat(1,prtUtilSubDir(fullfile(prtTarget,'engine','dataset'),'*.m','asdf'),prtUtilSubDir(fullfile(prtTarget,'engine'),'*.m','asdf'),{fullfile(prtTarget,'util','prtUtilCheckIsValidBeta.m')});
 for iFile = 1:length(filesToPCode)
     prtUtilPCode(filesToPCode{iFile});
 end
 
 % Copy over documenation
+disp('Copying documentation...');
 copyfile(fullfile(prtRoot,'doc'),fullfile(prtTarget,'doc'));
 rmdir(fullfile(prtTarget,'doc','.svn'),'s') % Delete .svn
 
@@ -64,15 +70,20 @@ if isdir(fullfile(prtTarget,'doc','helpsearch'))
 end
 
 % Copy the web doc over.
+disp('Copying web specific documentation...');
 docTarget = fullfile(prtTarget,'..','webDoc');
+if ~isdir(fullfile(docTarget))
+    mkdir(docTarget)
+end
 copyfile(fullfile(prtTarget,'doc'), docTarget);
 % Copy over the webOnlyDocFiles
 copyfile(fullfile(prtRoot,'internal','doc','docWebOnlyFiles'), docTarget);
 rmdir(fullfile(docTarget,'.svn'),'s') % Delete .svn
 
-
+disp('Zipping ...');
 zip(cat(2,prtTarget,'.zip'),prtTarget)
 
+disp('Done!');
 end
 
 function writeContents(root,version)
