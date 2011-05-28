c2f462aa85192f635f966c7c22a996a1110ca4af
sam <sam@0c11b213-6b92-4ae5-b10c-e53e59125080>
2011-05-28 02:03:45 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@972 0c11b213-6b92-4ae5-b10c-e53e59125080

---
+++ devel/internal/doc/prtDocDataSet.m
@@ -0,0 +1,92 @@
+%% Dataset
+% Before you can perform any actions with the PRT, you need to get your
+% data into a PRT dataset object.
+% 
+% There are 3 basic types of dataset objects, prtDataSetStandard,
+% prtDataSetClass, and prtDataSetRegress. The prtDataSetStandard object is
+% a generic object, containing much of the functionality needed to work
+% with the PRT. In general however, you will probably want to work with
+% either prtDataSetClass, or prtDataSetRegress, depending on if you are
+% doing classification or regression, respectively.
+
+%% prtDataSetStandard
+%
+%
+% prtDataSetStandard objects can hold either labeled or unlabled data. To
+% create am empty prtDataSetStandard object, you can use the following
+% command:
+
+ds = prtDataSetStandard
+
+%%
+%
+% The resulting object has no observations (nObservations = 0), no
+% features, and is not labeled. To assign some observations, you can use
+% the following command:
+
+ds = ds.setX([1; 2; 3])
+
+%%
+% This result of this command illustrates a few very important features of
+% the PRT to remember. First of all, if you want to set a property, you
+% need to say ds = ds.setX([1;2;3]). If you just say ds.set([1;2;3]), then
+% the result will be returned and stored in the ans variable, and the
+% original ds object will be unchanged.
+%
+% The second important feature this command illustrates is the orientation
+% of observations and features. Note that ds is now an object with 3
+% observations, since we passed setX a 3x1 column vector. So, each row of
+% the observation vector corresponds to a single observation. Each
+% observation can have multiple features, where each column corresponds to
+% a different feature. For example:
+
+ds = ds.setX([1 2; 3 4; 5 6])
+
+%%
+% ds is now a prtDataSetStandard object with 3 observations, and 2 features
+% per observation. The first observation, feature 1 equals 1, feature 2
+% equals 2, etc.
+%
+% Right now, the dataset is not labeled. To set labels, we can use the
+% setTargets function. Suppose we want to simply label the data as being a
+% member of class 0 or class 1, you can do the following:
+
+ds = ds.setTargets([1;0;1])
+
+%%
+% Note that the number of targets was equal to the number of observations.
+% This must be so, or else the setTargets function will error. You should
+% also see that the object is now labeled (isLabeled = 1), and the number
+% of target dimensions equals 1. In general, if your number of target
+% dimensions equals 1, then you are likely performing a classification, and
+% you should use the prtDataSetClass object. If your number of target
+% dimensions is greater than 1, you should probably be using the
+% prtDataSetRegress object.
+%
+% Finally, note that the ds object has fields observationInfo, and
+% featureInfo. These fields are to store whatever other information about
+% the observations or features that you might wish to carry around. For
+% example:
+
+ds = ds.setObservationInfo('info1',[7 8 9]')
+
+%%
+% Assigns a structure with 1 field named 'info1', where the value 7
+% corresponds to observation 1, etc. The same functionality works for
+% features:
+
+ ds = ds.setFeatureInfo('featInfo1', [11 12])
+ 
+ %%
+ % Where the value 11 corresponds to feature 1. Note, in general, it is not
+ % required to use the observationInfo or featureInfo fields. They just
+ % exist in case you have additional data that you wish to carry around,
+ % but do not necessarily want it to be processed by any of the PRT
+ % functions.
+ 
+ %% prtDataSetClass
+ 
+
+ 
+
+
---
+++ devel/internal/doc/prtDocUsersGuide.m
@@ -0,0 +1,1 @@
+% The Pattern Recognition Toolbox Users Guide
