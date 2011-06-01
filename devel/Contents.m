821248e76d15fc3963bda9c7f287d62eb204bdb0
sam <sam@0c11b213-6b92-4ae5-b10c-e53e59125080>
2011-06-01 02:47:52 +0000

    git-svn-id: http://svn.newfolderconsulting.com/prt@977 0c11b213-6b92-4ae5-b10c-e53e59125080

--- devel/Contents.m
+++ devel/Contents.m
@@ -1,2 +1,2 @@
 % Pattern Recognition Toolbox
-% Version 948 20-Apr-2011
+% Version 969 26-May-2011
---
+++ devel/internal/doc/prtDocClass.m
@@ -0,0 +1,78 @@
+%% PRT Classification Objects
+% One of the most powerful features of the Pattern Recognition Toolbox is
+% classification objects, implemented as <matlab:doc('prtClass') prtClass>
+% objects. Classification objects allow you to label data into clases.
+% prtClass objects are all supervised, meaning you have to train them
+% before you can perform classification.
+%
+%% Classification object methods and properties.
+% All prtClass objects inherit the TRAIN, RUN, CROSSVALIDATE and KFOLDS
+% functions from the prtAction object, for more information on these
+% methods, refer to section on the  <matlab:showdemo('prtDocEngine')
+% prtEngine>.
+%
+% In addition to the inherited methods, prtClass objects also have a few
+% important properties. The isNativeMary field indicates whether or not the
+% particular classifier is a binary classifier, or an M-ary classifier.
+% Binary classifiers can only label data as being in class 0 or 1, whereas
+% native M-ary classifiers can label data into an arbitrary number of
+% classes.
+%
+%% Internal Deciders
+% The other important property is the internalDecider. Ordinarily, a
+% prtClass object outputs raw statistics based on the classification
+% algorithm. However, you might just want the classification object to also
+% label the outputs. This can be done by setting the internalDecider
+% property. The example below will illustrate the use of a simple prtClass
+% object, with both the internalDecider unset and set.
+
+ds = prtDataGenUnimodal;   % Load a dataset to use
+classifier = prtClassGlrt  % Create a generalized likelihood ratio test classifier
+
+result = classifier.kfolds(ds,2);   % Perform a simple 2-fold cross-validation
+
+result.getX(1:5)
+
+%% 
+% Note that the data stored in the observations of result correspond to the
+% likelihood values. Also note that since ds was a labeled dataset, the
+% original labels are copied over into the targets property of the results
+% dataset.
+%
+% Now, set the internalDecider to be  a prtDecisionBinaryMinPe object:
+
+classifier.internalDecider = prtDecisionBinaryMinPe;
+result = classifier.kfolds(ds,2);   %  Perform a simple 2-fold cross-validation
+
+result.getX(1:5)
+
+% Note that now the data stored in the observations of result are class
+% labels. By setting the internalDecider to prtDecisionBinaryPe, an
+% threshold was found during training that would result in the minimum
+% probability of error.
+
+%% Plotting
+% Finally, prtClass objects all have an additional plot function, which can
+% help you visulize the classifiers decision regions. To plot the
+% classification object, it first needs to be trained. 
+
+classifier = classifier.train(ds);   % For example purposes, train with all the data
+classifier.plot();                   % Alternatively, plot(classifier) 
+
+%%
+% In the resulting plot, you will see all the data members used to train
+% the data. If the internalDecider is set, as in the above example, you
+% will see the decision region boundaries. If the internalDecider is not
+% set, you will instead see an intensity plot, indicating how likely it is
+% that a particular point would belong to class 0 or 1, as shown below.
+
+classifier.internalDecider = [];   % Clear the internalDecider
+classifier = classifier.train(ds); % Re-train
+classifier.plot()
+
+%%
+% All classification objects in the Pattern Recognition Toolbox have the
+% same API as discussed above. The only difference is the underlying
+% algorithms used to train and run the classifier. For a list of all the
+% different classification algorithms, and links to their individual help
+% entries, <prtDocFunctionList.html A list of commonly used functions>
