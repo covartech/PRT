b526d09f9493a2ee04f92af98d325b2408e9fc35
Kenneth Morton <kennethmorton@gmail.com>
2011-12-01 09:59:33 -0500

    replacing ~s with ]s as leading characters for things left off of the path by default

--- ~beta/brv/prtBrv.m
+++
@@ -1,9 +0,0 @@
-% PRTBRV - PRT Bayesian Random Variable Class
-%   Abstract Methods:
-%       nDimensions
-
-classdef prtBrv < prtAction
-    methods (Abstract)
-        val = nDimensions(obj)
-    end
-end
--- ~beta/prtDistanceHamming.m
+++
@@ -1,8 +0,0 @@
-function D = prtDistanceHamming(x,y)
-
-[x,y] = prtUtilDistanceParseInputs(x,y);
-
-x = logical(x);
-y = logical(y);
-
-D = sum(bsxfun(@xor,reshape(x,[size(x,1),1,size(x,2)]),reshape(y,[1,size(y,1),size(y,2)])),3);
