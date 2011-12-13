45554400a31b3f6cd6d7ad7e0b01c13c7bce3482
Peter Torrione <pete@newFolderConsulting.com>
2011-12-13 14:11:59 -0500

    added prtClassConstant, which uses the mean of the Labels; should be modified to use the mode for M-ary

---
+++ class/prtClassConstant.m
@@ -0,0 +1,36 @@
+classdef prtClassConstant < prtClass
+    
+    
+    properties (SetAccess=private)
+        
+        name = 'Constant' % Fisher Linear Discriminant
+        nameAbbreviation = 'Constant'            % FLD
+        isNativeMary = true;  % False
+    end
+    
+    properties
+        constant
+    end
+    
+    methods
+        % Allow for string, value pairs
+        function self = prtClassConstant(varargin)
+            self = prtUtilAssignStringValuePairs(self,varargin{:});
+        end
+        
+    end
+    
+    methods (Access=protected, Hidden = true)
+        
+        function self = trainAction(self,ds)
+            self.constant = mean(ds.getY);
+        end
+        
+        function ds = runAction(self,ds)
+            ds.X = repmat(self.constant,ds.nObservations,1);
+        end
+        
+        
+    end
+    
+end
