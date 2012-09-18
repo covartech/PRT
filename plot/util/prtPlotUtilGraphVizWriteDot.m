550e79a56e0e6597a3fa57739a30cc37d63007a8
Peter Torrione <pete@newFolderConsulting.com>
2012-09-18 16:40:16 -0400

    cleaning up MIL stuff

--- plot/util/prtPlotUtilGraphVizWriteDot.m
+++ plot/util/prtPlotUtilGraphVizWriteDot.m
@@ -12,8 +12,6 @@ else
     useNodeLabels = true;
 end
 
-
-
 fid = fopen(filename, 'w');
 
 C = onCleanup(@()fclose(fid)); % Even if we error
@@ -22,6 +20,7 @@ fprintf(fid, 'digraph G {\n');
 fprintf(fid, 'center = 1;\n');
 fprintf(fid, 'size=\"%d,%d\";\n', 10, 10);
 
+%Can speed this up, too
 nNodes = length(adj);
 for node = 1:nNodes
     if useNodeLabels
@@ -31,13 +30,24 @@ for node = 1:nNodes
     end
 end
 
-for node1 = 1:nNodes
-    arcs = find(adj(node1,:));
-    for node2 = arcs
-        fprintf(fid,'%d -> %d;\n',node1,node2);
-    end
-end
+%Much faster than below.
+[ii,jj] = find(adj);
+inds = sub2ind(size(adj),ii,jj);
+weights = full(adj(inds));
+edgeStrings = sprintf('%d -> %d [weight=%d];\n',[ii,jj,weights(:)]');
+fprintf(fid,'%s',edgeStrings);
 fprintf(fid, '}'); 
 
+%OnCleanup
+%fclose(fid);
 
+% for node1 = 1:nNodes
+%     arcs = find(adj(node1,:));
+%     
+%     for node2 = arcs
+%         fprintf(fid,'%d -> %d;\n',node1,node2);
+%     end
+% end
+% fprintf(fid, '}'); 
+%
 % fclose(fid); % On Cleanup will take care of it.
