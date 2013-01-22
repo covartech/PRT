7dfddea73f90657264703b6ea5ec1fccf9106e32
Peter Torrione <pete@newFolderConsulting.com>
2013-01-22 09:42:26 -0500

    added ]blogs

---
+++ ]blogs/torrione_2013.01.21_MNISTintro/torrione_blog_mnistIntro.m
@@ -0,0 +1,171 @@
+%% Introduction to the PRT: Using the MNIST data
+% The MNIST Database (http://yann.lecun.com/exdb/mnist/) is a very
+% well-known machine learning dataset consisting of a few thousand instances
+% of handwritten digits from 0-9.  MNIST is actually a subset of a larger
+% NIST database, but the authors (see the linked page above) were kind
+% enough to do some basic pre-processing of MNIST for us.  MNIST was for a
+% long time very widely used in the ML literature as an example of an easy
+% to use real data set to evaluate new ideas.
+
+%% Obtaining, Loading, and Visualizing MNIST Data
+% Tools to read in the MNIST database into the PRT are available in the
+% newest PRT version.  To conserve bandwidth, the actual MNIST data isn’t
+% included in the PRT (it would kill our subversion servers).  Instead you
+% can download the MNIST database from the website linked above, or from
+% our mirror, here: www.newfolderconsulting.com/data/.  Once you’ve
+% downloaded it, extract the data into:
+fullfile(prtRoot,'dataGen','dataStorage','MNIST') %MATLAB command will tell you the directory
+
+%% 
+% For example, on my system:
+ls(fullfile(prtRoot,'dataGen','dataStorage','MNIST'))
+
+%% 
+% Once the MNIST files are in the right place, execute the PRT command:
+dsTrain = prtDataGenMnist;
+%%
+% to extract the data.  ( Note, prtDataGenMnist makes use of a M-file
+% function called readMNIST by Siddharth Hegde.  It’s available from:
+% http://www.mathworks.com/matlabcentral/fileexchange/27675-read-digits-and-labels-from-mnist-database
+% ). 
+% 
+% Once loaded, we can use a number of different tools to visualize the
+% data.  First, let’s visualize the data as images.  We know that the
+% images are size 28x28, but since the prtDataSetClass object expects each
+% observation to correspond to a 1xN vector, we store all the 28x28 images
+% as 1x784 vectors.
+
+imageSize = [28 28];
+
+for i = 1:9;
+    subplot(3,3,i);
+    x = dsTrain.getX(i); %1x784
+    y = dsTrain.getY(i);
+    imagesc(reshape(x,imageSize));
+    colormap gray;
+    title(sprintf('MNIST; Digit = %d',y));
+end
+
+%% Classification: PLSDA 
+% What kinds of classification approaches can we apply to this data set?
+% We need to satisfy a few requirements: 1) M-Ary classification, 2)
+% Relatively fast, 3) Relatively insensitive to a large number of
+% dimensions (400 dimensional vectors). One particularly fast, linear
+% approach to classification that’s relatively insensitive to the number of
+% feature dimensions is partial-least squares discriminant analysis,
+% implemented in the PRT as prtClassPLSDA.  With only a few lines of code
+% we can implement and evaluate a PLSDA classifier on the MNIST data, for
+% example:
+
+algo = prtClassPlsda('nComponents',20) + prtDecisionMap; %we include the Max-A-Posteriori classifier
+yOut = algo.kfolds(dsTrain,3); %3 folds x-val
+pc = prtScorePercentCorrect(yOut);
+subplot(1,1,1);
+prtScoreConfusionMatrix(yOut);
+title(sprintf('3-Fold X-Val PLSDA on 10,000 MNIST Database Train Samples; %.0f%% Correct',pc*100));
+
+%% 
+% This basic example results in the above figure, where we see we’ve
+% achieved about 84% correct classification, and we can analyze confusions
+% between digits.  For example, the digits 4 and 9 are often confused,
+% which seems intuitive since they look relatively similar.
+% 
+% We can also evaluate the PLSDA classifier trained on 10,000 training
+% points and evaluated on the MNIST testing data.  To do so we first load
+% the testing data, then train our classifier and evaluate it:
+
+dsTest = prtDataGenMnistTest;
+algo = algo.train(dsTrain);
+yOut = algo.run(dsTest);
+
+pc = prtScorePercentCorrect(yOut);
+subplot(1,1,1);
+prtScoreConfusionMatrix(yOut);
+title(sprintf('PLSDA on 10,000 MNIST Database Test Samples; %.0f%% Correct',pc*100));
+
+%% 
+% Performance on the test set is relatively similar to performance in
+% cross-validation as can be seen above.
+% 
+% Overall, our performance is hovering around a 15% error rate.  That’s
+% roughly comparable to the 12% error reported in LeCun et al., 1988, and
+% here we’re not using a lot of the techniques in the Le Cun paper (and
+% this is with barely 5 lines of code!).
+
+%% Classification: SVM
+% As the results on http://yann.lecun.com/exdb/mnist/ illustrate, other
+% approaches to digit classification have done much better than our simple
+% PLSDA classifier.  We can use the PRT to apply more complicated
+% classifiers to the same data also, and hopefully decrease our error rate.
+
+%% 
+% For example, consider a simple application of an SVM classifier to the
+% digit recognition problem.  Since the SVM is not an M-ary classification
+% technique, we need to wrap our SVM in a One-Vs-All classifier to perform
+% M-ary classification (Warning: the following code took about 30 minutes
+% to run on my laptop):
+
+marySvm = prtPreProcZmuv + prtClassBinaryToMaryOneVsAll('baseClassifier',prtClassLibSvm) + prtDecisionMap;
+yOut = marySvm.kfolds(dsTrain,3);
+pc = prtScorePercentCorrect(yOut);
+subplot(1,1,1);
+prtScoreConfusionMatrix(yOut);
+title(sprintf('3-Fold X-Val SVM on 10,000 MNIST Database Train Samples; %.0f%% Correct',pc*100));
+
+%% 
+% As can be seen above, the SVM achieves an error rate of 5% on this data
+% set!  That’s a significant improvement over the PLSDA classification we
+% showed before.  Similarly to with PLSDA, we can also evaluate the
+% algorithm on completely separate testing data:
+
+marySvm = marySvm.train(dsTrain);
+yOut = marySvm.run(dsTest);
+
+pc = prtScorePercentCorrect(yOut);
+subplot(1,1,1);
+prtScoreConfusionMatrix(yOut);
+title(sprintf('PLSDA on 10,000 MNIST Database Test Samples; %.0f%% Correct',pc*100));
+
+%% 
+% And we see that performance is comparable to the cross-validated results.  
+% (Note that more advanced applications of SVM classifiers can do even better than the results reported here – Le Cun et al., 1998 reported 1.4% error rates with an SVM and some additional processing).
+
+%% Exploring the Results
+% If we wanted to improve classification, we could optimize over the SVM
+% parameters, kernel, pre-processing etc.  But before we did that, it might
+% be instructive to investigate what digits the SVM classifier is
+% mislabeling, and see if some of them seem like reasonable mistakes to
+% make.  The following code will pick 9 instances where the SVM output
+% label was different from the actual data label, and plot them in a
+% subplot.
+
+incorrect = find(yOut.getX ~= yOut.getY);
+yOutTestMisLabeled = yOut.retainObservations(incorrect);
+dsTestMisLabeled = dsTest.retainObservations(incorrect);
+for i = 1:9; %dsTestMisLabeled.nObservations;
+    randWrong = ceil(rand*dsTestMisLabeled.nObservations); %pick a random wrong element
+    subplot(3,3,i);
+    x = dsTestMisLabeled.getX(randWrong);
+    img = reshape(x,imageSize);
+    imagesc(img); 
+    colormap gray;
+    title(sprintf('Actual: %d; SVM Label: %d',yOutTestMisLabeled.getY(randWrong),yOutTestMisLabeled.getX(randWrong)));
+end
+
+%% 
+% Visual inspection of these mistakes illustrate some of the causes of
+% confusions in the SVM.  For example, highly slanted digits are often
+% mis-labeled.  Mitigating some of these mistakes may require significantly
+% more than simply optimizing SVM parameters!
+% 
+% Interested readers can refer to a large body of literature that has
+% previously investigated this data set (http://yann.lecun.com/exdb/mnist/)
+% for tips, tricks, and ideas for further improving performance on this
+% data set.  One particularly exciting recent advance is based on Hinton’s
+% deep learning networks, which enables very efficient learning on the
+% MNIST database (www.cs.toronto.edu/~hinton/science.pdf ).
+% 
+% We hope this example shows how quickly you can get from data to
+% results with the PRT.  Please let us know if you have comments or
+% questions!
+
---
+++ ]blogs/torrione_2013.01.22_CombiningActions/torrione_blog_combiningActions.m
@@ -0,0 +1,131 @@
+%% Using the PRT: Combining Actions
+% Hi!  Today I'd like to talk about how you can use the PRT to combine
+% actions together to form algorithms.  This is an important and powerful
+% tool in the PRT, and understanding it can solve a lot of headaches for
+% you.
+% 
+
+%% An Example
+% Let's start with a concrete example.  Say we want to classify some very
+% high dimensional data.  We'll start with the following:
+
+nFeatures = 200;
+ds = prtDataGenUnimodal;
+xNoise = randn(ds.nObservations,nFeatures);
+ds.X = cat(2,ds.X,xNoise); %add nFeatures meaningless features
+
+%%
+% If we try and classify this with a GLRT, for example, we're going to run
+% into trouble, since there are more features than there are observations,
+% so we can't generate a full-rank covariance structure.  For example,
+% using the prtAction prtClassGlrt, we might write this:
+glrt = prtClassGlrt;
+glrt = glrt.train(ds);
+try
+   yOut = glrt.run(ds);  %This causes errors
+catch ME
+    disp('Error encountered:')
+    disp(ME);
+end
+
+%%
+% We can always use dimension-reduction techniques to reduce the number of
+% features in our data set, and then evaluate performance.  For example:
+pca = prtPreProcPca('nComponents',2);
+pca = pca.train(ds);
+dsPca = pca.run(ds);
+plot(dsPca);
+
+%%
+% Now we can evaluate our GLRT on the dsPca:
+glrt = prtClassGlrt;
+yOutKfolds = glrt.kfolds(dsPca,10);
+[pf,pd] = prtScoreRoc(yOutKfolds);
+h = plot(pf,pd);
+set(h,'linewidth',3);
+title('Example GLRT ROC Curve (Running on PCA Features)');
+
+%% The Problem
+% There's a problem in the above, though.  Even though we cross-validated
+% the GLRT using 3 random folds, we didn't do the same thing with the PCA.
+% This is technically not fair, since the PCA part of the algorithm was
+% trained using all the data.  
+%
+% Maybe we can get around this like so:
+pca = prtPreProcPca('nComponents',2);
+dsPca = pca.kfolds(ds,10);
+%%
+% But now, when we do:
+glrt = prtClassGlrt;
+yOutKfolds = glrt.kfolds(dsPca,10);
+[pf,pd] = prtScoreRoc(yOutKfolds);
+h = plot(pf,pd);
+set(h,'linewidth',3);
+title('This is no good...');
+%%
+% We have a problem!  At every fold, we learn a unique set of PCA loadings.
+% Since PCA loadings have arbitrary sign (+/-), the outputs across all
+% these folds will overlap!
+plot(dsPca)
+%% 
+% The underlying problem is that there's no guarantee that the folds used
+% for PCA and GLRT evaluation were the same.  We can get around *that* if
+% we specified the folds, and wrote our own cross-validate specifically for
+% this new process we've made, but suddenly this is getting complicated.
+%
+% And what if we had an even more complicated process, including other
+% pre-processing streams, feature selection, classifiers and
+% decision-makers?  Suddenly our code is going to be a mess!
+
+%% Combining Actions into Algorithms
+% At the heart of the problem outlined above is that the PCA and GLRT parts
+% of our process weren't considered as two parts of the same process - they
+% were two separate variables, and the PRT and MATLAB didn't know that they
+% should work together.
+%
+% Since this problem is so common, the PRT provides an easy way to combine
+% each individual part of a process (prtActions) into one big process (a
+% prtAlgorithm).  This is easily done using the "+" operator:
+pcaGlrt = prtPreProcPca('nComponents',2) + prtClassGlrt;
+
+%%
+% If you're not used to object oriented programming, the above might look a
+% little weird.  But it's straightforward - we've defined "plus" ("+") for
+% prtActions (e.g., prtPreProcPca) to mean "Combine these into one object,
+% where that object will perform each action in sequence from left to
+% right".  Technically this returns a special kind of prtAction, called a
+% prtAlgorithm.  That's just the date type we use to store a bunch of
+% actions.  You can see that here:
+disp(pcaGlrt)
+%% 
+% You can visualize the structure of the algorithm using PLOT:
+plot(pcaGlrt)
+%%
+% You can combine any number of prtActions into an algorithm like this, so,
+% although its silly, this is technically a valid command:
+sillyAlgo = prtPreProcZmuv + prtPreProcHistEq + prtPreProcPca + prtClassGlrt;
+plot(sillyAlgo)
+
+%% Using Algorithms
+% So we've made a prtAlgorithm.  Now what?  Well, anything you can do to a
+% prtAction, you can do with a prtAlgorithm.  What does that mean?  Methods
+% like plot, kfolds, and crossValidate all work exactly the same as they do
+% with regular prtActions.  And they make your life much simpler than what
+% we had to do above:
+pcaGlrt = prtPreProcPca('nComponents',2) + prtClassGlrt;
+yOutKfolds = pcaGlrt.kfolds(ds,10);
+[pf,pd] = prtScoreRoc(yOutKfolds);
+h = plot(pf,pd);
+set(h,'linewidth',3);
+title('ROC Curve for a prtAlgorithm (PCA + GLRT)');
+
+%% 
+% The results in the ROC curve above were generated using 10-folds
+% cross-validation on the combination of PCA and GLRT.  At each fold,
+% 9/10ths of the data were used to train the PCA and GLRT, and 1/10th was
+% used for evaluation.  
+%
+% prtAlgorithms are a very powerful tool for pattern recognition, and we
+% hope this blog post helps clear up how to make and use them!  
+%
+% Let us know if you have any questions or comments.
