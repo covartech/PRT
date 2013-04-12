d2bd20c466b186601426f14391c024b6536dcf38
Peter Torrione <pete@newFolderConsulting.com>
2013-04-12 10:01:49 -0400

    mod to prtPath for depoloyed versions

--- prtPath.m
+++ prtPath.m
@@ -23,8 +23,9 @@ function prtPath(varargin)
 % OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 % USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-
-startupCheck = true;
+if isdeployed;
+    return;
+end
 if startupCheck
     checkPrtInStartup;
 end
