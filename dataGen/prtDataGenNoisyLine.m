d4ebf29db3c3fd855e305f6eb898163dc0f240f1
Patrick Wang <patrick.wang@duke.edu>
2013-07-30 13:20:11 -0400

    documentation for preRegressPlsda

--- dataGen/prtDataGenNoisyLine.m
+++ dataGen/prtDataGenNoisyLine.m
@@ -1,9 +1,14 @@
-function DataSet = prtDataGenNoisyLine
+function DataSet = prtDataGenNoisyLine(varargin)
 % prtDataGenNoisyLine Generates noisy line example data
 %
 %   DATASET = prtDataGenNoisyLine returns a prtDataSetRegress with 100
-%   samples of a line with zero-mean additive Gaussian noise. The noise
+%   samples of a line on x = [-1, 1] with zero-mean additive Gaussian
+%   noise. By default, the slope is 1 and the y-intercept is 0. The noise
 %   variance is .1.
+%
+%   DATASET = prtDataGenNoisyLine(slope, y_intercept) returns a
+%   prtDataSetRegress with 100 samples of a line on x = [-1, 1] with
+%   zero-mean additive Gaussian noise. The noise variance is .1.
 %
 %   Example:
 %
@@ -33,15 +38,20 @@ function DataSet = prtDataGenNoisyLine
 % OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 % USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-
-
+if nargin~=2
+	slope = 1;
+	y_intercept = 0;
+else
+	slope = varargin{1};
+	y_intercept = varargin{2};
+end
 
 nSamples = 100;
 noiseVar = 0.1;
 
 t = linspace(-1,1,1000);
 x = prtUtilRandSample(t,nSamples)';
-t = x;
+t = x*slope + y_intercept;
 y = t + noiseVar*randn(size(x));
 
 DataSet = prtDataSetRegress(x,y,'name','Noisy Line');
