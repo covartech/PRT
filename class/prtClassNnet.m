2bbb1ac8006cddbeb36c4f3257ae9ce6c16c9dfc
Peter Torrione <pete@newFolderConsulting.com>
2013-08-07 12:39:49 -0400

    added nnet code

---
+++ class/prtClassNnet.m
@@ -0,0 +1,157 @@
+classdef prtClassNnet < prtClass
+    %prtClassNnet 3-Layer Neural Network Classifier
+    %
+    %    nnet = prtClassNnet; returns a neural network classifier with 3
+    %    layers (input, hidden, output).
+    %
+    %    nnet = prtClassNnet(PROPERTY1, VALUE1, ...) constructs a
+    %    prtClassNnet object with properties as specified by
+    %    PROPERTY/VALUE pairs.
+    %
+    %    A prtClassFld object inherits all properties from the abstract class
+    %    prtClass. In addition is has the following properties:
+    %
+    %    nHiddenUnits (10)  - Number of hidden units to use
+    %    minIters  (10000)  - Minimum number of iterations to run before
+    %                         checking for convergence
+    %    maxIters (100000)  - Maximum number of iterations to run
+    %    
+    %    relativeErrorChangeThreshold (1e-4) - Relative change in error to
+    %                       determine convergence
+    %
+    %    initStdev (0.1)   - Standard deviation to use for intial weights
+    %    stepSize  (0.1)   - Learning stepsize
+    %    fwdFn (sigmoid)   - Activation function for nodes; see prtUtilNnetBackProp
+    %    fwdFnDeriv        - Activation function derivative defaults to
+    %                         derivative of sigmoid; see prtUtilNnetBackProp
+    %
+    %    plotOnIter (0)    - Scalar integer specifying how often to
+    %                         visualize results and convergence
+    % Example:
+    %    dsTrain = prtDataGenXor;
+    %    dsTest = prtDataGenXor;
+    %    nnet = prtClassNnet('nHiddenUnits',10,'plotOnIter',1000,'relativeErrorChangeThreshold',1e-4);
+    %    nnet = nnet.train(dsTrain);
+    %    yOut = nnet.run(dsTest);
+    %    close all;
+    %    prtScoreRoc(yOut);
+    %
+    % Notes:
+    %   Currently, prtClassNnet implements a simple back-propagation
+    %   algorithm as described in Duda, Hart, Stork, "Pattern
+    %   Classification", 2nd Ed., Pages 291-293.  More complicated
+    %   algorithms are also possible.
+    %
+    %   The current code is only suitable for binary classification
+    %   problems. M-ary modifications are possible and on our to-do-list.
+    %    
+    
+    properties (SetAccess=private)
+        name = 'NNET'
+        nameAbbreviation = 'NNET' 
+        isNativeMary = false; 
+    end
+    
+    properties (SetAccess = protected)
+        
+    end
+    
+    properties
+        nHiddenUnits = 30;
+        
+        minIters = 10000;
+        maxIters = 100000;
+        relativeErrorChangeThreshold = 1e-4;
+        
+        initStdev = 0.1;
+        stepSize = 0.1;
+        fwdFn = [];
+        fwdFnDeriv = [];
+        
+        plotOnIter = false;
+        
+        initWeights = true;
+        weightCell = {};
+    end
+    
+    methods
+     
+               % Allow for string, value pairs
+        function self = prtClassNnet(varargin)
+            self = prtUtilAssignStringValuePairs(self,varargin{:});
+        end
+    end
+    
+    methods (Access=protected, Hidden = true)
+        
+        function self = trainAction(self,dataSet)
+            
+            nFeaturesOrig = dataSet.nFeatures;
+            %Add bias:
+            dataSet.X = cat(2,ones(dataSet.nObservations,1),dataSet.X);
+            if self.initWeights
+                wCell = {randn(dataSet.nFeatures,self.nHiddenUnits).*self.initStdev,randn(self.nHiddenUnits+1,1).*self.initStdev};
+            else
+                %Use the weights from last-time. Note: this will error if
+                %you're not careful
+                wCell = self.weightCell;
+                if isempty(wCell)
+                    error('prt:prtClassNnet:initWeights','nnet.initWeights is false, but previous weightCell is empty.');
+                end
+            end
+            
+            meanError = nan(1,self.maxIters);
+            converged = false;
+            
+            for iter = 1:self.maxIters;
+                [wCell,out] = prtUtilNnetBackProp(dataSet,wCell,self.stepSize,self.fwdFn,self.fwdFnDeriv);
+                meanError(iter) = mean((out - dataSet.targets).^2);
+                
+                % Force there to be at least minIters, then check
+                % convergence
+                if iter > self.minIters
+                    percentChange = abs((meanError(iter)-meanError(iter-1))./(meanError(iter-1)));
+                    converged = percentChange < self.relativeErrorChangeThreshold;
+                end
+                if converged
+                    percentChange
+                    break;
+                end
+                
+                
+                % Plotting:
+                if ~mod(iter,self.plotOnIter);
+                    if nFeaturesOrig < 4
+                        subplot(2,2,1);
+                        self.isTrained = true;
+                        self.weightCell = wCell;
+                        plot(self);
+                        %                         set(gca,'clim',[0 1]);
+                        subplot(2,2,2);
+                        plot(1:length(out),out,1:length(out),dataSet.targets);
+                        subplot(2,1,2);
+                        plot(log10(meanError));
+                        drawnow;
+                    else
+                        subplot(2,1,2);
+                        plot(1:length(out),out,1:length(out),dataSet.targets);
+                        subplot(2,1,1);
+                        plot(log10(meanError));
+                    end
+                end
+            end
+            self.weightCell = wCell;
+        end
+        
+        function dataSet = runAction(self,dataSet)
+            % dataSet = runAction(self,dataSet)
+            
+            %Add bias:
+            dataSet.X = cat(2,ones(dataSet.nObservations,1),dataSet.X);
+            [~,out] = prtUtilNnetBackProp(dataSet,self.weightCell,self.stepSize,self.fwdFn,self.fwdFnDeriv);
+            dataSet.X = out;
+        end
+        
+    end
+    
+end
---
+++ util/prtUtilNnetBackProp.m
@@ -0,0 +1,83 @@
+function [weightCellOut,out] = prtUtilNnetBackProp(ds,weightCell,stepSize,fwdFn,fwdFnDeriv)
+%[weightCell,out] = prtUtilNnetBackProp(ds,weightCell)
+%[weightCell,out] = prtUtilNnetBackProp(ds,weightCell,stepSize)
+%[weightCell,out] = prtUtilNnetBackProp(ds,weightCell,stepSize,fwdFn,fwdFnDeriv)
+%   Perform back-propagation on the weights in weightCell, using the data
+%   in ds, the appropriate stepSize and activation functions.  
+%
+%   Defaults:
+%       stepSize = 0.1
+%       fwdFn = @(x) 1./(1 + exp(-x));
+%       fwdFnDeriv = @(x) fwdFn(x).*(1-fwdFn(x)); %true for sigmoid
+%       
+%
+% Based on Duda, Hart, Stork, "Pattern Classification", 2nd Ed., Pages
+% 291-293.
+%
+% Currently:
+%  Does both forward and back-prop (should be separated)
+%  Only valid for single-target column (e.g., binary classsification)
+%  
+
+sigmoidFn = @(x) 1./(1 + exp(-x));
+if nargin < 3 || isempty(stepSize)
+    stepSize = .1;
+end
+if nargin < 4 || isempty(fwdFn)
+    fwdFn = sigmoidFn;
+end
+if nargin < 5 || isempty(fwdFnDeriv)
+    fwdFnDeriv = @(x) sigmoidFn(x).*(1-sigmoidFn(x));
+end
+
+%Initialize output 
+weightCellOut = weightCell;
+
+% Forward run:
+input = {ds.X};
+n = size(input{1},1);
+
+for i = 1:length(weightCell)
+    net{i+1} = (input{i}*weightCell{i});
+    input{i+1} = fwdFn(net{i+1});
+    % Bias:
+    input{i+1} = cat(2,ones(n,1),input{i+1});
+end
+% Remove bias from output
+input{end} = input{end}(:,2:end);
+out = input{end};
+
+%Handle nan targets
+if isempty(ds.targets)
+    ds.targets = nan(n,1);
+end
+
+% Update last set of weights, see, Duda, Page 291
+
+% for nn = 1:n
+%     for k = 1
+%         for j = 1:31
+%             dwkj(nn,k,j) = stepSize*(eRaw(nn))*fwdFnDeriv(net{end}(nn))*input{end-1}(nn,j);
+%         end
+%     end
+% end
+eRaw = ds.targets-input{end};
+deltaK = eRaw.*fwdFnDeriv(net{end});
+dw = stepSize*bsxfun(@times,deltaK,input{end-1});
+
+weightCellOut{end} = weightCellOut{end} + mean(dw)';
+
+% Update first set of weights, see, Duda, Page 292
+
+% for nn = 1:n
+%     for i=1:3;
+%         for j = 1:30
+%             dwji(nn,j,i) = stepSize*(weightCell{end}(j+1)*deltaK(nn))*fwdFnDeriv(net{2}(nn,j))*input{1}(nn,i);
+%         end
+%     end
+% end
+deltaJ = bsxfun(@times,weightCell{2}(2:end)',deltaK).*fwdFnDeriv(net{2});
+for i = 1:size(input{1},2)
+    dw = stepSize*bsxfun(@times,deltaJ,input{1}(:,i));
+    weightCellOut{1}(i,:) = weightCellOut{1}(i,:) + mean(dw);
+end
