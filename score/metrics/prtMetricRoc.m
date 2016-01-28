8b422cf403df8ff73325cbe4b56d41e3aaf9842f
CoVar Research <covarresearch001@gmail.com>
2016-01-28 10:00:51 -0500

    prtMetricRoc find pd at pf/far values

--- score/metrics/prtMetricRoc.m
+++ score/metrics/prtMetricRoc.m
@@ -25,6 +25,67 @@ classdef prtMetricRoc
             val = self.nfa./self.farDenominator;
         end
         
+        function [meanRoc,stdRoc,farVals] = getRocFarStatistics(self,farVals)
+            % [meanRoc,stdRoc] = getRocStatistics(self,nPoints)
+            % [meanRoc,stdRoc] = getRocStatistics(self,farVals)
+            % get mean & std of a bunch of ROCs
+            if nargin < 2
+                farVals = 250;
+            end
+            
+            if numel(farVals) == 1
+                nPoints = 250;
+                allFar = cat(1,self(:).far);
+                farVals = linspace(0,max(allFar),nPoints);
+            end
+            
+            pdVals = self.pdAtFarValues(farVals);
+            pdVals = cat(2,pdVals{:});
+            
+            meanRoc = nanmean(pdVals,2);
+            stdRoc = nanstd(pdVals,[],2);
+            
+        end
+        
+        function pdOut = pdAtFarValues(self,farPoints)
+            if numel(self)>1
+                pdOut = cell(size(self));
+                for iSelf = 1:numel(self)
+                    pdOut{iSelf} = self(iSelf).pdAtFarValues(farPoints);
+                end
+                return
+            end
+            
+            tmpFar = self.far;
+            tmpFar(end+1) = Inf;
+            tmpPd = self.pd;
+            tmpPd(end+1) = nan;
+            
+            indOut = arrayfun(@(s)find(tmpFar>s,1),farPoints);
+            %ind = find(self.far > farPoints,1,'first');
+            pdOut = tmpPd(indOut);
+        end
+        
+        function pdOut = pdAtPfValues(self,pfPoints)
+            if numel(self)>1
+                pdOut = cell(size(self));
+                for iSelf = 1:numel(self)
+                    pdOut{iSelf} = self(iSelf).pdAtPfValues(pfPoints);
+                end
+                return
+            end
+            
+            % return nan values if past ROC curve
+            tmpPf = self.pf;
+            tmpPf(end+1) = Inf;
+            tmpPd = self.pd;
+            tmpPd(end+1) = nan;
+            
+            indOut = arrayfun(@(s)find(tmpPf>s,1),pfPoints);
+            %ind = find(self.far > farPoints,1,'first');
+            pdOut = tmpPd(indOut);
+        end
+        
         function self = atThreshold(self,threshold)
             
             index = find(self.tau > threshold,1,'last');
