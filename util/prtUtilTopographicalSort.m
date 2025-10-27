3c205778d79ac9596131c226fddffe01550b277a
Kenny Morton <kenny@covar.com>
2025-10-27 16:45:04 -0400

    Update in topological sort for new colon behavior

--- util/prtUtilTopographicalSort.m
+++ util/prtUtilTopographicalSort.m
@@ -6,16 +6,16 @@ function seq = prtUtilTopographicalSort(adj)
 %
 % Taken from Bayesian Network Toolbox by Minka
 
-N = size(adj);
+N = size(adj,1);
 indeg = sum(adj,1);
 outdeg = sum(adj,2);
 seq = [];
-for i = 1:N,
+for i = 1:N
     idx = find(indeg==0);    % Find nodes with indegree 0
-    if isempty(idx),   % If can't find than graph contains a cycle
+    if isempty(idx)   % If can't find than graph contains a cycle
         seq = [];
         break;
-    end;
+    end
     [dummy, idx2] = max(outdeg(idx)); % Remove the node with the max number of connections
     indx = idx(idx2);
     seq = [seq, indx];
